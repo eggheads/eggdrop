f7e5e69d91a6d7ca5607a151f0aea23848a095bc
Fabian Knittel <fabian.knittel@avona.com>
1999-12-25 00:36:15 +0000

    tclasyncdns.patch

---
+++ src/dns.h
@@ -0,0 +1,51 @@
+/* 
+ * dns.h
+ *   stuff used by dns.c
+ * 
+ * $Id: dns.h,v 1.1 1999/12/25 00:36:15 fabian Exp $
+ */
+/* 
+ * Written by Fabian Knittel <fknittel@gmx.de>
+ *
+ * Copyright (C) 1999  Eggheads
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _EGG_DNS_H
+#define _EGG_DNS_H
+
+typedef struct {
+  char *name;
+  int (*expmem)(void *);
+  void (*event)(IP, char *, int, void *);
+} devent_type;
+
+typedef struct devent_str {
+  struct devent_str *next;	/* Pointer to next dns_event	   */
+  devent_type	*type;
+  byte		lookup;
+  time_t	timeout;	/* Timeout of this dns request	   */
+  union {
+    IP		ip_addr;	/* IP address			   */
+    char	*hostname; 	/* Hostname			   */
+  } res_data;
+  union {
+    char	*proc;		/* Tcl proc			   */
+    void	*other;
+  } misc;
+} devent_t;
+
+#endif	/* _EGG_DNS_H */
