5717d5154a11f80a06028a261009ecf4a789618d
Fabian Knittel <fabian.knittel@avona.com>
2000-09-05 16:00:30 +0000

    fabian: whois_match_crash patch

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  * 
- * $Id: patch.h,v 1.299 2000/09/05 15:59:43 fabian Exp $
+ * $Id: patch.h,v 1.300 2000/09/05 16:00:30 fabian Exp $
  */
 /* 
  * Copyright (C) 1997  Robey Pointer
@@ -40,11 +40,11 @@ patch("CVS");		/* CVS beta version */
 /*
  *
  */
-patch("968172294");	/* current unixtime */
+patch("968172347");	/* current unixtime */
 /*
  *
  */
-patch("minor_share_cleanup");
+patch("whois_match_crash");
 /*
  *
  */
--- src/users.c
+++ src/users.c
@@ -10,7 +10,7 @@
  * 
  * dprintf'ized, 9nov1995
  * 
- * $Id: users.c,v 1.17 2000/08/06 14:51:38 fabian Exp $
+ * $Id: users.c,v 1.18 2000/09/05 16:00:30 fabian Exp $
  */
 /* 
  * Copyright (C) 1997  Robey Pointer
@@ -512,9 +512,9 @@ void tell_user(int idx, struct userrec *u, int master)
       else {
 	now2 = now - (ch->laston);
 	if (now2 > 86400)
-	  strftime(s1, 7, "%d %b", localtime(&li->laston));
+	  strftime(s1, 7, "%d %b", localtime(&ch->laston));
 	else
-	  strftime(s1, 6, "%H:%M", localtime(&li->laston));
+	  strftime(s1, 6, "%H:%M", localtime(&ch->laston));
       }
       fr.match = FR_CHAN;
       fr.chan = ch->flags;
