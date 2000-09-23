91b59054bc7e04e1045b3208a1ee3dce1aec0e1b
Fabian Knittel <fabian.knittel@avona.com>
2000-09-23 17:47:50 +0000

    fabian: applied seen_help patch

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  * 
- * $Id: patch.h,v 1.322 2000/09/23 17:46:55 fabian Exp $
+ * $Id: patch.h,v 1.323 2000/09/23 17:47:50 fabian Exp $
  */
 /* 
  * Copyright (C) 1997  Robey Pointer
@@ -40,11 +40,11 @@ patch("CVS");		/* CVS beta version */
 /*
  *
  */
-patch("969733944");	/* current unixtime */
+patch("969734002");	/* current unixtime */
 /*
  *
  */
-patch("transfer_turbodcc_dos");
+patch("seen_help");
 /*
  *
  */
