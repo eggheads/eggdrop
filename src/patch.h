b249a0282d9ea7fd617a94f2d804f3972bead9b7
Fabian Knittel <fabian.knittel@avona.com>
2000-09-23 17:49:07 +0000

    fabian: applied eugene's helppath (aka eggdrop.simple.conf) patch

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  * 
- * $Id: patch.h,v 1.323 2000/09/23 17:47:50 fabian Exp $
+ * $Id: patch.h,v 1.324 2000/09/23 17:49:07 fabian Exp $
  */
 /* 
  * Copyright (C) 1997  Robey Pointer
@@ -40,11 +40,11 @@ patch("CVS");		/* CVS beta version */
 /*
  *
  */
-patch("969734002");	/* current unixtime */
+patch("969734079");	/* current unixtime */
 /*
  *
  */
-patch("seen_help");
+patch("helppath");
 /*
  *
  */
