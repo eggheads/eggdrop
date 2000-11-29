fe3982330c8d79f753423401ce83eaf635652f07
Jeff Fisher <guppy@techmonkeys.org>
2000-11-29 02:49:20 +0000

    part of sup's variable fix patch.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  * 
- * $Id: patch.h,v 1.392 2000/11/29 02:46:16 guppy Exp $
+ * $Id: patch.h,v 1.393 2000/11/29 02:49:20 guppy Exp $
  */
 /* 
  * Copyright (C) 1997  Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("975465081");		/* current unixtime */
+patch("975465363");		/* current unixtime */
 /*
  *
  *
  */
-patch("thread-stuff");
+patch("variablefix");
 /*
  *
  *
