c9d5c7a7c7f399dadaef6ffe50c254e8c494cb7d
Jeff Fisher <guppy@techmonkeys.org>
2001-04-13 19:25:05 +0000

    more typos and more German translations have been added

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.458 2001/04/13 19:19:14 guppy Exp $
+ * $Id: patch.h,v 1.459 2001/04/13 19:25:05 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("987190502");		/* current unixtime */
+patch("987190983");		/* current unixtime */
 /*
  *
  *
  */
-patch("weed");
+patch("germantranslations2");
 /*
  *
  *
