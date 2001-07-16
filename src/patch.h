d3032048ec69a8381faf44093bbe6b62fcf21fe7
Jeff Fisher <guppy@techmonkeys.org>
2001-07-16 14:53:21 +0000

    applied drummer's patch to get rid of remove_gunk

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.518 2001/07/16 05:14:33 guppy Exp $
+ * $Id: patch.h,v 1.519 2001/07/16 14:53:21 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("995260328");		/* current unixtime */
+patch("995295137");		/* current unixtime */
 /*
  *
  *
  */
-patch("whoisfix");
+patch("remove_gunk");
 /*
  *
  *
