ba88f3c324e834d33f7e8b6d4204378cc01bdbec
Jeff Fisher <guppy@techmonkeys.org>
2001-07-24 15:08:22 +0000

    here is Kirben's bind82.patch applied

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.531 2001/07/24 15:02:01 guppy Exp $
+ * $Id: patch.h,v 1.532 2001/07/24 15:08:22 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("995986884");		/* current unixtime */
+patch("995987197");		/* current unixtime */
 /*
  *
  *
  */
-patch("ban_reason");
+patch("bind82");
 /*
  *
  *
