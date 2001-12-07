cde66f280df0c798505befce62cffea134ad7240
Matthew Hallacy <poptix@poptix.net>
2001-12-07 18:20:30 +0000

    Hah, slipping this in, makes action.fix handle irssi (and a few others) "broken" behavior.

--- scripts/action.fix.tcl
+++ scripts/action.fix.tcl
@@ -2,13 +2,19 @@
 #
 # Tothwolf  25May1999: cleanup
 # Tothwolf  04Oct1999: changed proc names slightly
+# poptix    07Dec2001: handle irssi (and some others) "correct" messages for DCC CTCP
 #
-# $Id: action.fix.tcl,v 1.4 2001/11/05 04:08:28 guppy Exp $
+# $Id: action.fix.tcl,v 1.5 2001/12/07 18:20:30 poptix Exp $
 
 # fix for mIRC dcc chat /me's
 bind filt - "\001ACTION *\001" filt:dcc_action
+bind filt - "CTCP_MESSAGE \001ACTION *\001" filt:dcc_action2
 proc filt:dcc_action {idx text} {
   return ".me [string trim [join [lrange [split $text] 1 end]] \001]"
+}
+
+proc filt:dcc_action2 {idx text} {
+  return ".me [string trim [join [lrange [split $text] 2 end]] \001]"
 }
 
 # fix for telnet session /me's
