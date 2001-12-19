06285f569918e9a63660f811fcc998423ac2243f
Jeff Fisher <guppy@techmonkeys.org>
2001-12-19 04:58:39 +0000

    added Wcc's doc updates for BHAB's serverdocfix patch

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.631 2001/12/19 04:57:54 guppy Exp $
+ * $Id: patch.h,v 1.632 2001/12/19 04:58:39 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1008737245");		/* current unixtime */
+patch("1008737812");		/* current unixtime */
 /*
  *
  *
  */
-patch("small-fixes");
+patch("servdocfix2");
 /*
  *
  *
