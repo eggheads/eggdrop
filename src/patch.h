2878121ac3ded239b6583a6e54d1a4410518cc46
Jeff Fisher <guppy@techmonkeys.org>
2002-01-03 01:05:43 +0000

    added Wcc's latest doc (wasn't sent via email since his stmp is busted)

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.665 2002/01/02 20:23:40 guppy Exp $
+ * $Id: patch.h,v 1.666 2002/01/03 01:05:43 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1010002514");		/* current unixtime */
+patch("1010019616");		/* current unixtime */
 /*
  *
  *
  */
-patch("tclhashhack");
+patch("htmldocs");
 /*
  *
  *
