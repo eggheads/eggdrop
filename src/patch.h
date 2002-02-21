91749508e47d42ffa7570420f97d52cdaeb6d984
Jeff Fisher <guppy@techmonkeys.org>
2002-02-21 05:55:11 +0000

    forgot patch.h

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.692 2002/02/20 22:32:35 guppy Exp $
+ * $Id: patch.h,v 1.693 2002/02/21 05:55:11 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1014244155");		/* current unixtime */
+patch("1014270737");		/* current unixtime */
 /*
  *
  *
  */
-patch("solarisfix");
+patch("removechans");
 /*
  *
  *
