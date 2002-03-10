a370c6f3e3b1042a6c9a5b51b2177e64baa387d7
Will Buckner <wcc@techmonkeys.org>
2002-03-10 18:10:28 +0000

    Added a few channels to the README and readme.html and made references to other docs in the html files links to their respective html files.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.720 2002/03/10 17:34:31 guppy Exp $
+ * $Id: patch.h,v 1.721 2002/03/10 18:10:28 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1015781741");		/* current unixtime */
+patch("1015783987");		/* current unixtime */
 /*
  *
  *
  */
-patch("modesperline");
+patch("channels_docs");
 /*
  *
  *
