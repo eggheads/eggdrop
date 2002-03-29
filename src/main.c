dc2c904225d3b94048f5aa305f443eba7153f0ff
Will Buckner <wcc@techmonkeys.org>
2002-03-29 20:47:47 +0000

    1.6.11

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
  *   command line arguments
  *   context and assert debugging
  *
- * $Id: main.c,v 1.81 2002/03/11 05:17:01 guppy Exp $
+ * $Id: main.c,v 1.82 2002/03/29 20:47:47 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -81,8 +81,8 @@ extern jmp_buf		 alarmret;
  * modified versions of this bot.
  */
 
-char	egg_version[1024] = "1.6.10";
-int	egg_numver = 1061000;
+char	egg_version[1024] = "1.6.11";
+int	egg_numver = 1061100;
 
 char	notify_new[121] = "";	/* Person to send a note to for new users */
 int	default_flags = 0;	/* Default user flags and */
--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.736 2002/03/29 05:53:55 guppy Exp $
+ * $Id: patch.h,v 1.737 2002/03/29 20:47:47 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1017381225");		/* current unixtime */
+patch("1017434577");		/* current unixtime */
 /*
  *
  *
  */
-patch("protectfixes");
+patch("none");
 /*
  *
  *
