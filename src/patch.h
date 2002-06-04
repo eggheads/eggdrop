15143658c00aa776ff3840c9234177e0f20a2296
Will Buckner <wcc@techmonkeys.org>
2002-06-04 07:02:46 +0000

    Ok, lets try now.. \:P

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.756 2002/06/04 05:03:26 wcc Exp $
+ * $Id: patch.h,v 1.757 2002/06/04 07:02:46 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1022544350");		/* current unixtime */
+patch("1023174135");		/* current unixtime */
 /*
  *
  *
  */
-patch("miscdoc");
+patch("miscdoc3");
 /*
  *
  *
