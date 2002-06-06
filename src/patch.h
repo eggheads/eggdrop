d5cc584df55700ae8fd84acd40c5cda9dbb82acd
Will Buckner <wcc@techmonkeys.org>
2002-06-06 03:11:01 +0000

    I'm sure that one day, I'll eventually finish this.. sigh.. :P

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.757 2002/06/04 07:02:46 wcc Exp $
+ * $Id: patch.h,v 1.758 2002/06/06 03:11:01 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1023174135");		/* current unixtime */
+patch("1023333015");		/* current unixtime */
 /*
  *
  *
  */
-patch("miscdoc3");
+patch("miscdoc4");
 /*
  *
  *
