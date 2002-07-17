fa99b52fd76ddc491e100fa1f4ee944409618d7c
Will Buckner <wcc@techmonkeys.org>
2002-07-17 19:18:29 +0000

    Added 'strip' to tcl-commands.[doc|html] and made the description for NOTC more clear.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.775 2002/07/10 00:37:36 wcc Exp $
+ * $Id: patch.h,v 1.776 2002/07/17 19:18:29 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1026261201");		/* current unixtime */
+patch("1026933407");		/* current unixtime */
 /*
  *
  *
  */
-patch("debugandfilt-docs");
+patch("stripandnotc-docs");
 /*
  *
  *
