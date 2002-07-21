387a1e66dfb5d5c5c04b8f39e2c695d3b1d2c129
Will Buckner <wcc@techmonkeys.org>
2002-07-21 23:56:43 +0000

    Small help file fix for -ban (found by MC_8 - bug #261).

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.781 2002/07/21 02:39:04 wcc Exp $
+ * $Id: patch.h,v 1.782 2002/07/21 23:56:43 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1027219066");		/* current unixtime */
+patch("1027295707");		/* current unixtime */
 /*
  *
  *
  */
-patch("halfopdocs");
+patch("mns_ban_help");
 /*
  *
  *
