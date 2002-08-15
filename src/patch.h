8fb53f886dad6116a88b99e39a3204e3fafd130f
Will Buckner <wcc@techmonkeys.org>
2002-08-15 01:45:46 +0000

    Forgot to run autoconf.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.806 2002/08/15 00:01:01 wcc Exp $
+ * $Id: patch.h,v 1.807 2002/08/15 01:45:46 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1029369573");		/* current unixtime */
+patch("1029375989");		/* current unixtime */
 /*
  *
  *
  */
-patch("tcl-rpath");
+patch("tcl-rpath2");
 /*
  *
  *
