c2d45e18fd122cf3a87517ef1400581ab45ff6ae
Will Buckner <wcc@techmonkeys.org>
2002-10-11 01:39:46 +0000

    Forgot patch.h on last commit.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.837 2002/10/08 04:25:37 wcc Exp $
+ * $Id: patch.h,v 1.838 2002/10/11 01:39:46 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1034048227");		/* current unixtime */
+patch("1034300142");		/* current unixtime */
 /*
  *
  *
  */
-patch("modes_per_line_max");
+patch("strftimefix");
 /*
  *
  *
