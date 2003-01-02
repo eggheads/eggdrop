084c55026e5ddc717b6aac076e6b825e557c51fe
Will Buckner <wcc@techmonkeys.org>
2003-01-02 02:42:53 +0000

    Doc updates from past commit.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.866 2003/01/02 00:07:46 wcc Exp $
+ * $Id: patch.h,v 1.867 2003/01/02 02:42:53 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1041465885");		/* current unixtime */
+patch("1041475323");		/* current unixtime */
 /*
  *
  *
  */
-patch("wallopsfix");
+patch("wallopsfix2");
 /*
  *
  *
