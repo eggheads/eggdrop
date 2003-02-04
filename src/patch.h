6b75e39bfbbd4a2ea6ccbc6ac455fed97e191c2e
Will Buckner <wcc@techmonkeys.org>
2003-02-04 11:11:11 +0000

    * Forgot patch.h.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.908 2003/02/04 11:07:55 wcc Exp $
+ * $Id: patch.h,v 1.909 2003/02/04 11:11:11 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1044356855");            /* current unixtime */
+patch("1044357068");            /* current unixtime */
 /*
  *
  *
  */
-patch("stricthostfx");
+patch("realname");
 /*
  *
  *
