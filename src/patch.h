f0fe1f30a074fb32fd3a5b82b2dc5545e9e2a0c1
Will Buckner <wcc@techmonkeys.org>
2003-02-21 02:15:58 +0000

    * Don't use the help queue for dcc sends or gets. Found by winkey.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.921 2003/02/18 00:47:57 wcc Exp $
+ * $Id: patch.h,v 1.922 2003/02/21 02:15:57 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1045529108");            /* current unixtime */
+patch("1045793411");            /* current unixtime */
 /*
  *
  *
  */
-patch("tracednick_case");
+patch("dccqueue");
 /*
  *
  *
