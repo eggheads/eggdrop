a609564f6b196374193bbe02ed681f31d152f97c
Will Buckner <wcc@techmonkeys.org>
2003-05-13 20:06:49 +0000

    * Tcl commands doc update by PPSlim.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.996 2003/05/12 23:57:51 wcc Exp $
+ * $Id: patch.h,v 1.997 2003/05/13 20:06:49 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1052783820");            /* current unixtime */
+patch("1052856282");            /* current unixtime */
 /*
  *
  *
  */
-patch("durationspaces2");
+patch("channel-add-doc");
 /*
  *
  *
