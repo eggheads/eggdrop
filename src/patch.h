a009d3a56cf52da11e747adff230bde1a5fda548
Will Buckner <wcc@techmonkeys.org>
2004-04-09 18:00:31 +0000

    * Missing \n in some notes files. Found by dollar.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1067 2004/04/09 04:18:07 wcc Exp $
+ * $Id: patch.h,v 1.1068 2004/04/09 18:00:31 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1081484251");            /* current unixtime */
+patch("1081533616");            /* current unixtime */
 /*
  *
  *
  */
-patch("tcl8.5");
+patch("noteslang");
 /*
  *
  *
