9461f8888e89398103d05df7a7d76f436bcfcac0
Will Buckner <wcc@techmonkeys.org>
2004-06-10 20:03:19 +0000

    - Typo in doc/COMPILING-FAQ. It's LD_LIBRARY_PATH, not LB_LIBRARY_PATH. * Found by: NoPleX - Patch by: Wcc

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1087 2004/06/09 17:34:16 wcc Exp $
+ * $Id: patch.h,v 1.1088 2004/06/10 20:03:19 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1086787974");            /* current unixtime */
+patch("1086896678");            /* current unixtime */
 /*
  *
  *
  */
-patch("sscanf-fix");
+patch("compile-faq");
 /*
  *
  *
