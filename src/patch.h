9ed5386f98b747997d4dc7e5109684193ca48ffb
Will Buckner <wcc@techmonkeys.org>
2004-06-15 07:25:51 +0000

    * Ran autoconf (/). * Modules: configure.in -> configure.ac.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1101 2004/06/15 07:20:55 wcc Exp $
+ * $Id: patch.h,v 1.1102 2004/06/15 07:25:51 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1087278754");            /* current unixtime */
+patch("1087284321");            /* current unixtime */
 /*
  *
  *
  */
-patch("configurecrap");
+patch("configurecrap2");
 /*
  *
  *
