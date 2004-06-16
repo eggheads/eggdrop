8f91d6fe3771f7adbce842b1d7b119f20223488b
Will Buckner <wcc@techmonkeys.org>
2004-06-16 06:55:16 +0000

    * Ran autoconf && autoheader.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1103 2004/06/15 07:28:14 wcc Exp $
+ * $Id: patch.h,v 1.1104 2004/06/16 06:55:16 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1087284395");            /* current unixtime */
+patch("1087368908");            /* current unixtime */
 /*
  *
  *
  */
-patch("configurecrap3");
+patch("configurecrap5");
 /*
  *
  *
