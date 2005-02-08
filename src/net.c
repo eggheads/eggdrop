3991d4f9b0c636d190f7c8002df1fe8961b80d58
Tothwolf <tothwolf@techmonkeys.org>
2005-02-08 00:14:35 +0000

    - Corrected copyright header for net.c.   (I thought I did this years ago when I did all the others...)

--- src/net.c
+++ src/net.c
@@ -2,11 +2,25 @@
  * net.c -- handles:
  *   all raw network i/o
  *
- * $Id: net.c,v 1.73 2004/08/28 06:50:35 wcc Exp $
+ * $Id: net.c,v 1.74 2005/02/08 00:14:35 tothwolf Exp $
  */
 /*
- * This is hereby released into the public domain.
- * Robey Pointer, robey@netcom.com
+ * Copyright (C) 1997 Robey Pointer
+ * Copyright (C) 1999 - 2005 Eggheads Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 #include <fcntl.h>
