dddb091976d05e79527b91b1ef2eebda7e084895
Will Buckner <wcc@techmonkeys.org>
2005-08-16 02:11:17 +0000

    * Ran misc/runautotools.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1160 2005/07/31 17:37:29 wcc Exp $
+ * $Id: patch.h,v 1.1161 2005/08/16 02:11:17 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1122831364");            /* current unixtime */
+patch("1124158256");            /* current unixtime */
 /*
  *
  *
  */
-patch("nat-ip-docs");
+patch("OSX-dns");
 /*
  *
  *
