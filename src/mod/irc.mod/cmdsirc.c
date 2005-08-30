ffa689483621993129ee9afb19d919cbc286008e
Will Buckner <wcc@techmonkeys.org>
2005-08-30 01:52:49 +0000

    - No need to fill the buffers at the end of the cmd_[de]voice() functions. * Patch by: lordares

--- src/mod/irc.mod/cmdsirc.c
+++ src/mod/irc.mod/cmdsirc.c
@@ -2,7 +2,7 @@
  * chancmds.c -- part of irc.mod
  *   handles commands directly relating to channel interaction
  *
- * $Id: cmdsirc.c,v 1.58 2005/01/03 20:01:46 paladin Exp $
+ * $Id: cmdsirc.c,v 1.59 2005/08/30 01:52:49 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -562,7 +562,6 @@ static void cmd_voice(struct userrec *u, int idx, char *par)
     dprintf(idx, "%s is not on %s.\n", nick, chan->dname);
     return;
   }
-  egg_snprintf(s, sizeof s, "%s!%s", m->nick, m->userhost);
   add_mode(chan, '+', 'v', nick);
   dprintf(idx, "Gave voice to %s on %s\n", nick, chan->dname);
 }
@@ -617,7 +616,6 @@ static void cmd_devoice(struct userrec *u, int idx, char *par)
     dprintf(idx, "%s is not on %s.\n", nick, chan->dname);
     return;
   }
-  egg_snprintf(s, sizeof s, "%s!%s", m->nick, m->userhost);
   add_mode(chan, '-', 'v', nick);
   dprintf(idx, "Devoiced %s on %s\n", nick, chan->dname);
 }
--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1167 2005/08/29 23:44:33 wcc Exp $
+ * $Id: patch.h,v 1.1168 2005/08/30 01:52:49 wcc Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1125359134");            /* current unixtime */
+patch("1125365750");            /* current unixtime */
 /*
  *
  *
  */
-patch("timeout-param-cast");
+patch("useless-uhost");
 /*
  *
  *
