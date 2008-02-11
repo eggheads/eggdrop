851488c06cc5ec3b89f6a098d37d22a2b975a039
Jeff Fisher <guppy@techmonkeys.org>
2008-02-11 01:23:14 +0000

    and the script for the previous commit

---
+++ scripts/quotepass.tcl
@@ -0,0 +1,37 @@
+#
+# quotepass.tcl
+#   written by simple, stdarg, and guppy
+#
+# Some servers on the Undernet will make you send 'PASS <numbers>' before you
+# can connect if you did not return an identd response. This script will
+# handle sending that for you.
+#
+# updates
+# -------
+#  10Feb08: initial version
+#
+# $Id: quotepass.tcl,v 1.1 2008/02/11 01:23:14 guppy Exp $
+
+bind evnt - init-server quotepass_unbind
+bind evnt - disconnect-server quotepass_unbind
+bind evnt - connect-server quotepass_bind
+
+proc quotepass_notice {from cmd text} {
+  if {[regexp "PASS (.*)" $text - pass]} {
+    putserv "PASS $pass"
+  }
+  return 0
+}
+
+proc quotepass_unbind {type} {
+  # Try to unbind our raw NOTICE bind once we are connected since it will
+  # never be needed again 
+  catch {
+    unbind raw - NOTICE quotepass_notice
+  }
+}
+
+proc quotepass_bind {type} {
+  bind raw - NOTICE quotepass_notice
+} 
+
