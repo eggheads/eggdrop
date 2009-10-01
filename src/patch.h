9c27b7f406a8c526d6cc264eaaab8e2bd02c0da3
Rumen Stoyanov <pseudo@egg6.net>
2009-10-01 14:38:04 +0000

    Updated msg/irc.help to include /MSG commands available to halfops. Fixes Bugzilla Bug # 475 "halfop and dehalfop missing from message help display"
    Patch by: pseudo

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.1206 2009/09/22 20:30:47 thommey Exp $
+ * $Id: patch.h,v 1.1207 2009/10/01 14:38:04 pseudo Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1253651127");            /* current unixtime */
+patch("1254406257");            /* current unixtime */
 /*
  *
  *
  */
-patch("keylogfix");
+patch("halfophelp");
 /*
  *
  *
