d1bac3f10e387cd5af68977c69c3367be1227d91
Rumen Stoyanov <pseudo@egg6.net>
2010-10-24 12:39:36 +0000

    Removed wire.mod and all references to it. Botnet and partyline encryption are now available using ssl.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.14 2010/10/23 11:16:12 pseudo Exp $
+ * $Id: patch.h,v 1.15 2010/10/24 12:39:36 pseudo Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1287832418");            /* current unixtime */
+patch("1287923885");            /* current unixtime */
 /*
  *
  *
  */
-patch("binddie");
+patch("wireless");
 /*
  *
  *
