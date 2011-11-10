cc01b56213bc40ad184b9d0aa3301be3ea6c73f9
Thomas Sader <thommey@gmail.com>
2011-11-10 22:10:10 +0000

    Fixed the format specifier for the Tcl [traffic] command.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.25.2.17 2011/10/25 16:49:19 thommey Exp $
+ * $Id: patch.h,v 1.25.2.18 2011/11/10 22:10:10 thommey Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1319561302");            /* current unixtime */
+patch("1320962988");            /* current unixtime */
 /*
  *
  *
  */
-patch("nicklentrace");
+patch("unsignedtraffic");
 /*
  *
  *
