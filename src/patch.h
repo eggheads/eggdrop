de1c688e9c897b532f124ebca270d68495066af2
Thomas Sader <thommey@gmail.com>
2012-12-10 22:54:15 +0000

    Fix match_cidr to always return MATCH if the prefix is 0.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.25.2.24 2012/09/25 17:24:55 thommey Exp $
+ * $Id: patch.h,v 1.25.2.25 2012/12/10 22:54:15 thommey Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1348593615");            /* current unixtime */
+patch("1355179806");            /* current unixtime */
 /*
  *
  *
  */
-patch("modtclresult");
+patch("cidrzeromatch");
 /*
  *
  *
