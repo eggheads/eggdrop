9b50119fec003d2cf20a815546babbecdba486e8
Thomas Sader <thommey@gmail.com>
2014-09-30 10:40:40 +0000

    Add Tcl8.6 and /usr/lib/x86_64-linux-gnu to Tcl search paths.

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.50 2014/03/21 21:42:41 thommey Exp $
+ * $Id: patch.h,v 1.51 2014/09/30 10:40:40 thommey Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");                   /* CVS version */
  *
  *
  */
-patch("1395438000");            /* current unixtime */
+patch("1412073609");            /* current unixtime */
 /*
  *
  *
  */
-patch("fixbotnetloop");
+patch("archlibpath");
 /*
  *
  *
