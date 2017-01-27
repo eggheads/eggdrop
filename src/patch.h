8fb917492c009913a7a16c9e1cda7bd4f35d9a68
Geo <george.vo@alum.rpi.edu>
2017-01-26 21:21:37 -0500

    Update patchlevel to timerdrift

--- src/patch.h
+++ src/patch.h
@@ -39,12 +39,12 @@ patch("Git");                   /* Git version */
  *
  *
  */
-patch("1479772226");            /* current unixtime */
+patch("1485483674");            /* current unixtime */
 /*
  *
  *
  */
-patch("tlslinkpass");
+patch("timerdrift");
 /*
  *
  *
