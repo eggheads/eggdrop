7616ba6695c33eba2898799a404e7d5222bfa547
Geo <george.vo@alum.rpi.edu>
2017-02-03 12:46:51 -0500

    Update patchlevel to remotewho

--- src/patch.h
+++ src/patch.h
@@ -39,12 +39,12 @@ patch("Git");                   /* Git version */
  *
  *
  */
-patch("1485398611");            /* current unixtime */
+patch("1486143995");            /* current unixtime */
 /*
  *
  *
  */
-patch("vhostreply");
+patch("remotewho");
 /*
  *
  *
