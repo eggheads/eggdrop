d6165e29ef52683d1a071fb265521552f521066b
Geo <vanosg@users.noreply.github.com>
2017-02-12 20:37:04 -0600

    Fixes for autobotchk. Fixes #239
    Found by: Cizzle, StephenS
    Patch by: Cizzle, Geo
    
    * adds possibilty for multiple confs if settings are split
    * fixes nick or botnet-nick with \\[ \\] in creating botchk file not findable by cron
    * fixes multiple cron entries added when \\[ \\] is in filename
    * fixes cases where botnet-nick="$nick" or pidfile="pid.${botnet-nick}" or any  of the other vars nick, userfile, pidfile and botnet-nick having one of the other vars in conf
    * fixes cases where botnet-nick or nick has handlen > defined handlen and pidfile uses this botnet-nick or nick
    * Prevent \s from being used in filenames
    * Fix addt'l handlen, {}, and pidfile autobotchk issues
    * Update copyright info

--- src/patch.h
+++ src/patch.h
@@ -39,12 +39,12 @@ patch("Git");                   /* Git version */
  *
  *
  */
-patch("1486176263");            /* current unixtime */
+patch("1486503373");            /* current unixtime */
 /*
  *
  *
  */
-patch("sockgets");
+patch("autobotchk");
 /*
  *
  *
