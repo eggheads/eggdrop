05d16c665433a0953eec0db74519c597a3a736f1
Christophe Beauval <Cizzle@users.noreply.github.com>
2017-04-20 00:54:11 +0200

    Use ASN1_STRING_get0_data instead of deprecated ASN1_STRING_data in OpenSSL 1.1.0+. Also change modifiers to use const.

--- src/proto.h
+++ src/proto.h
@@ -306,7 +306,7 @@ int readtclprog(char *fname);
 #ifdef TLS
 int ssl_handshake(int, int, int, int, char *, IntFunc);
 char *ssl_fpconv(char *in, char *out);
-char *ssl_getuid(int sock);
+const char *ssl_getuid(int sock);
 char *ssl_getfp(int sock);
 #endif
 
