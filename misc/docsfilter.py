5052ee5f279431d8d4d1b94d1e931ac6f0830ce5
Geo <geo@eggheads.org>
2017-04-19 08:39:15 -0400

    Update pandoc filter for new anchor-style docs

--- misc/docsfilter.py
+++ misc/docsfilter.py
@@ -2,15 +2,17 @@
 
 from panflute import *
 
+def upper(elem, doc):
+    if isinstance(elem, Str):
+        elem.text = elem.text.upper()
+
 def action(elem, doc):
     if isinstance(elem, Header):
-        elem.level=1
-    if isinstance(elem, Strong):
-        return list(elem.content)
+        if elem.level == 2:
+            return elem.walk(upper)
 
 def main(doc=None):
     return run_filter(action, doc=doc)
 
 if __name__ == "__main__":
     main()
-
