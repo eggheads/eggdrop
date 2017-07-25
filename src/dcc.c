beb367e74d58c4a8673ac7eb94439684a2f2beeb
Christophe Beauval <Cizzle@users.noreply.github.com>
2017-07-26 01:04:45 +0200

    Implement logging outgoing botnet traffic and outgoing share traffic.

--- src/dcc.c
+++ src/dcc.c
@@ -465,9 +465,9 @@ static void dcc_bot(int idx, char *code, int i)
 
   if (raw_log) {
     if (!strcmp(code, "s"))
-      putlog(LOG_BOTSHARE, "*", "{%s} %s", dcc[idx].nick, code + 2);
+      putlog(LOG_BOTSHARE, "*", "{m<-%s} %s", dcc[idx].nick, code + 2);
     else
-      putlog(LOG_BOTNETIN, "*", "[<%s] %s", dcc[idx].nick, code);
+      putlog(LOG_BOTNETIN, "*", "[m<-%s] %s", dcc[idx].nick, code);
   }
   msg = strchr(code, ' ');
   if (msg) {
@@ -527,6 +527,33 @@ static void display_dcc_fork_bot(int idx, char *buf)
   sprintf(buf, "conn  bot");
 }
 
+static void out_dcc_bot(int idx, char *buf, void *x)
+{
+  size_t len = strlen(buf);
+  /* We don't really use x here, so "use it" for the compiler */
+  (void)x;
+
+  if (raw_log) {
+    /* strip \n from end as putlog appends this */
+    char fnd = 0;
+    if (buf[len - 1] == '\n') {
+      fnd = 1;
+      buf[len - 1] = 0;
+    }
+
+    if (!strncmp(buf, "s ", 2))
+      putlog(LOG_BOTSHARE, "*", "{m->%s} %s", dcc[idx].nick, buf + 2);
+    else
+      putlog(LOG_BOTNETOUT, "*", "[m->%s] %s", dcc[idx].nick, buf);
+
+    /* Re-append \n if present */
+    if (fnd)
+      buf[len - 1] = '\n';
+  }
+
+  tputs(dcc[idx].sock, buf, strlen(buf));
+}
+
 struct dcc_table DCC_BOT = {
   "BOT",
   DCT_BOT | DCT_VALIDIDX,
@@ -537,7 +564,7 @@ struct dcc_table DCC_BOT = {
   display_dcc_bot,
   expmem_dcc_bot_,
   free_dcc_bot_,
-  NULL
+  out_dcc_bot
 };
 
 struct dcc_table DCC_FORK_BOT = {
