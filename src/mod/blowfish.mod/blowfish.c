5b644ace891867d6db554a5648070b0081aa8e34
orkim <d.orkim@gmail.com>
2017-08-02 22:11:27 -0500

    fixed header include and formatted blowfish module

--- src/mod/blowfish.mod/blowfish.c
+++ src/mod/blowfish.mod/blowfish.c
@@ -27,9 +27,9 @@
 #define MODULE_NAME "encryption"
 #define MAKING_ENCRYPTION
 
-#include "src/mod/module.h"
 #include "blowfish.h"
-#include "bf_tab.h"             /* P-box P-array, S-box */
+#include "bf_tab.h" /* P-box P-array, S-box */
+#include "src/mod/module.h"
 
 #undef global
 static Function *global = NULL;
@@ -45,7 +45,7 @@ static char bf_mode[4];
 #define S2(x) (bf_S[2][x.w.byte2])
 #define S3(x) (bf_S[3][x.w.byte3])
 #define bf_F(x) (((S0(x) + S1(x)) ^ S2(x)) + S3(x))
-#define ROUND(a,b,n) (a.word ^= bf_F(b) ^ bf_P[n])
+#define ROUND(a, b, n) (a.word ^= bf_F(b) ^ bf_P[n])
 
 /* Keep a set of rotating P & S boxes */
 static struct box_t {
@@ -61,8 +61,7 @@ static struct box_t {
 static u_32bit_t *bf_P;
 static u_32bit_t **bf_S;
 
-static int blowfish_expmem()
-{
+static int blowfish_expmem() {
   int i, tot = 0;
 
   for (i = 0; i < BOXES; i++)
@@ -74,8 +73,7 @@ static int blowfish_expmem()
   return tot;
 }
 
-static void blowfish_encipher(u_32bit_t *xl, u_32bit_t *xr)
-{
+static void blowfish_encipher(u_32bit_t *xl, u_32bit_t *xr) {
   union aword Xl;
   union aword Xr;
 
@@ -105,8 +103,7 @@ static void blowfish_encipher(u_32bit_t *xl, u_32bit_t *xr)
   *xl = Xr.word;
 }
 
-static void blowfish_decipher(u_32bit_t *xl, u_32bit_t *xr)
-{
+static void blowfish_decipher(u_32bit_t *xl, u_32bit_t *xr) {
   union aword Xl;
   union aword Xr;
 
@@ -136,9 +133,7 @@ static void blowfish_decipher(u_32bit_t *xl, u_32bit_t *xr)
   *xr = Xl.word;
 }
 
-
-static void blowfish_report(int idx, int details)
-{
+static void blowfish_report(int idx, int details) {
   if (details) {
     int i, tot = 0, size = blowfish_expmem();
 
@@ -158,8 +153,7 @@ static void blowfish_report(int idx, int details)
   }
 }
 
-static void blowfish_init(u_8bit_t *key, int keybytes)
-{
+static void blowfish_init(u_8bit_t *key, int keybytes) {
   int i, j, bx;
   time_t lowest;
   u_32bit_t data;
@@ -177,7 +171,7 @@ static void blowfish_init(u_8bit_t *key, int keybytes)
   for (i = 0; i < BOXES; i++)
     if (box[i].P != NULL) {
       if ((box[i].keybytes == keybytes) &&
-          (!strncmp((char *) (box[i].key), (char *) key, keybytes))) {
+          (!strncmp((char *)(box[i].key), (char *)key, keybytes))) {
         /* Match! */
         box[i].lastuse = now;
         bf_P = box[i].P;
@@ -216,7 +210,7 @@ static void blowfish_init(u_8bit_t *key, int keybytes)
   bf_P = box[bx].P;
   bf_S = box[bx].S;
   box[bx].keybytes = keybytes;
-  strncpy(box[bx].key, (char *) key, keybytes);
+  strncpy(box[bx].key, (char *)key, keybytes);
   box[bx].key[keybytes] = 0;
   box[bx].lastuse = now;
   /* Robey: Reset blowfish boxes to initial state
@@ -230,7 +224,7 @@ static void blowfish_init(u_8bit_t *key, int keybytes)
       bf_S[i][j] = initbf_S[i][j];
 
   j = 0;
-  if (keybytes > 0) {           /* drummer: fixes crash if key=="" */
+  if (keybytes > 0) { /* drummer: fixes crash if key=="" */
     for (i = 0; i < bf_N + 2; ++i) {
       temp.word = 0;
       temp.w.byte0 = key[j];
@@ -261,17 +255,16 @@ static void blowfish_init(u_8bit_t *key, int keybytes)
 /* Of course, if you change either of these, then your userfile will
  * no longer be able to be shared. :)
  */
-#define SALT1  0xdeadd061
-#define SALT2  0x23f6b095
+#define SALT1 0xdeadd061
+#define SALT2 0x23f6b095
 
 /* Convert 64-bit encrypted password to text for userfile */
 static char *base64 =
-            "./0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    "./0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 static char *cbcbase64 =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-static int base64dec(char c)
-{
+static int base64dec(char c) {
   int i;
 
   for (i = 0; i < 64; i++)
@@ -280,24 +273,22 @@ static int base64dec(char c)
   return 0;
 }
 
-static int cbcbase64dec(char c)
-{
+static int cbcbase64dec(char c) {
   char *i = strchr(cbcbase64, c);
   return i ? (int)(i - cbcbase64) : -1;
 }
 
-static void blowfish_encrypt_pass(char *text, char *new)
-{
+static void blowfish_encrypt_pass(char *text, char *new) {
   u_32bit_t left, right;
   int n;
   char *p;
 
-  blowfish_init((unsigned char *) text, strlen(text));
+  blowfish_init((unsigned char *)text, strlen(text));
   left = SALT1;
   right = SALT2;
   blowfish_encipher(&left, &right);
   p = new;
-  *p++ = '+';                   /* + means encrypted pass */
+  *p++ = '+'; /* + means encrypted pass */
   n = 32;
   while (n > 0) {
     *p++ = base64[right & 0x3f];
@@ -315,8 +306,7 @@ static void blowfish_encrypt_pass(char *text, char *new)
 
 /* Returned string must be freed when done with it!
  */
-static char *encrypt_string_ecb(char *key, char *str)
-{
+static char *encrypt_string_ecb(char *key, char *str) {
   u_32bit_t left, right;
   unsigned char *p;
   char *s, *dest, *d;
@@ -327,14 +317,14 @@ static char *encrypt_string_ecb(char *key, char *str)
   strcpy(s, str);
   if ((!key) || (!key[0]))
     return s;
-  p = (unsigned char *) s;
+  p = (unsigned char *)s;
   dest = nmalloc((strlen(str) + 9) * 2);
   while (*p)
     p++;
   for (i = 0; i < 8; i++)
     *p++ = 0;
-  blowfish_init((unsigned char *) key, strlen(key));
-  p = (unsigned char *) s;
+  blowfish_init((unsigned char *)key, strlen(key));
+  p = (unsigned char *)s;
   d = dest;
   while (*p) {
     left = ((*p++) << 24);
@@ -362,8 +352,7 @@ static char *encrypt_string_ecb(char *key, char *str)
 
 /* Returned string must be freed when done with it!
  */
-static char *encrypt_string_cbc(char *key, char *str)
-{
+static char *encrypt_string_cbc(char *key, char *str) {
   u_32bit_t left, right, prevleft = 0, prevright = 0;
   unsigned char *p;
   char *s, *dest;
@@ -374,21 +363,21 @@ static char *encrypt_string_cbc(char *key, char *str)
   slen = strlen(str) + 8;
   s = nmalloc(slen + 9);
   for (i = 0; i < 8; ++i) {
-    s[i] = (char) (random() % 256);
+    s[i] = (char)(random() % 256);
   }
   strcpy(s + 8, str);
   if ((!key) || (!key[0]))
     return s;
-  p = (unsigned char *) s + slen;
+  p = (unsigned char *)s + slen;
   while (slen % 8) {
     ++slen;
     *p++ = 0;
   }
   *p = 0;
 
-  blowfish_init((unsigned char *) key, strlen(key));
+  blowfish_init((unsigned char *)key, strlen(key));
 
-  p = (unsigned char *) s;
+  p = (unsigned char *)s;
   /* p[0] can be 0 as it's random IV so check if at start of IV */
   while (*p || (p == (unsigned char *)s)) {
     left = ((*p++) << 24);
@@ -412,10 +401,10 @@ static char *encrypt_string_cbc(char *key, char *str)
 
     /* turn back into chars */
     for (i = 0; i < 32; i += 8) {
-        *--p = (unsigned char) ((right >> i) & 0xff);
+      *--p = (unsigned char)((right >> i) & 0xff);
     }
     for (i = 0; i < 32; i += 8) {
-        *--p = (unsigned char) ((left >> i) & 0xff);
+      *--p = (unsigned char)((left >> i) & 0xff);
     }
     p += 8;
   }
@@ -427,20 +416,21 @@ static char *encrypt_string_cbc(char *key, char *str)
 
   /* base64 encode */
   /* go on til slen - #possible pads */
-  p = (unsigned char *) dest + 1;
+  p = (unsigned char *)dest + 1;
   for (i = 0; i < slen - 2; i += 3) {
-    *p++ = cbcbase64[((unsigned char) s[i]) >> 2];
+    *p++ = cbcbase64[((unsigned char)s[i]) >> 2];
     *p++ = cbcbase64[((s[i] & 0x03) << 4) | (((unsigned char)s[i + 1]) >> 4)];
-    *p++ = cbcbase64[((s[i + 1] & 0x0f) << 2) | (((unsigned char)s[i + 2]) >> 6)];
+    *p++ =
+        cbcbase64[((s[i + 1] & 0x0f) << 2) | (((unsigned char)s[i + 2]) >> 6)];
     *p++ = cbcbase64[s[i + 2] & 0x3f];
   }
   if (slen - i == 2) {
-    *p++ = cbcbase64[((unsigned char) s[i]) >> 2];
+    *p++ = cbcbase64[((unsigned char)s[i]) >> 2];
     *p++ = cbcbase64[((s[i] & 0x03) << 4) | (((unsigned char)s[i + 1]) >> 4)];
     *p++ = cbcbase64[(s[i + 1] & 0x0f) << 2];
     *p++ = '=';
   } else if (slen - i == 1) {
-    *p++ = cbcbase64[((unsigned char) s[i]) >> 2];
+    *p++ = cbcbase64[((unsigned char)s[i]) >> 2];
     *p++ = cbcbase64[(s[i] & 0x03) << 4];
     *p++ = '=';
     *p++ = '=';
@@ -454,8 +444,7 @@ static char *encrypt_string_cbc(char *key, char *str)
 
 /* Returned string must be freed when done with it!
  */
-static char *encrypt_string(char *key, char *str)
-{
+static char *encrypt_string(char *key, char *str) {
   if (!egg_strncasecmp(key, "ecb:", 4)) {
     return encrypt_string_ecb(key + 4, str);
 
@@ -467,7 +456,6 @@ static char *encrypt_string(char *key, char *str)
 
   } else if (!egg_strncasecmp(bf_mode, "cbc", sizeof bf_mode)) {
     return encrypt_string_cbc(key, str);
-
   }
 
   /* else ECB for now, change at v1.9.0! */
@@ -476,8 +464,7 @@ static char *encrypt_string(char *key, char *str)
 
 /* Returned string must be freed when done with it!
  */
-static char *decrypt_string_ecb(char *key, char *str)
-{
+static char *decrypt_string_ecb(char *key, char *str) {
   u_32bit_t left, right;
   char *p, *s, *dest, *d;
   int i;
@@ -493,7 +480,7 @@ static char *decrypt_string_ecb(char *key, char *str)
     p++;
   for (i = 0; i < 12; i++)
     *p++ = 0;
-  blowfish_init((unsigned char *) key, strlen(key));
+  blowfish_init((unsigned char *)key, strlen(key));
   p = s;
   d = dest;
   while (*p) {
@@ -516,8 +503,7 @@ static char *decrypt_string_ecb(char *key, char *str)
 
 /* Returned string must be freed when done with it!
  */
-static char *decrypt_string_cbc(char *key, char *str)
-{
+static char *decrypt_string_cbc(char *key, char *str) {
   u_32bit_t left, right, prevleft = 0, prevright = 0, prevencleft, prevencright;
   unsigned char *p;
   char *s, *dest;
@@ -530,12 +516,12 @@ static char *decrypt_string_cbc(char *key, char *str)
   if ((!key) || (!key[0]) || (slen % 4))
     return s;
 
-  blowfish_init((unsigned char *) key, strlen(key));
+  blowfish_init((unsigned char *)key, strlen(key));
 
   /* base64 decode */
   dlen = (slen >> 2) * 3;
   dest = nmalloc(dlen + 1);
-  p = (unsigned char *) dest;
+  p = (unsigned char *)dest;
   /* '=' will/should return 64 */
   for (i = 0; i < slen; i += 4) {
     int s1 = cbcbase64dec(s[i]);
@@ -544,11 +530,11 @@ static char *decrypt_string_cbc(char *key, char *str)
     int s4 = cbcbase64dec(s[i + 3]);
     if (s1 < 0 || s1 == 64 || s2 < 0 || s2 == 64 || s3 < 0 || s4 < 0)
       return s;
-    *p++ = (unsigned char) ((s1 << 2) | (s2 >> 4));
+    *p++ = (unsigned char)((s1 << 2) | (s2 >> 4));
     if (s3 != 64) {
-      *p++ = (unsigned char) ((s2 << 4) | (s3 >> 2));
+      *p++ = (unsigned char)((s2 << 4) | (s3 >> 2));
       if (s4 != 64)
-        *p++ = (unsigned char) ((s3 << 6) | s4);
+        *p++ = (unsigned char)((s3 << 6) | s4);
       else
         --dlen;
     } else
@@ -560,7 +546,7 @@ static char *decrypt_string_cbc(char *key, char *str)
   if (dlen % 8)
     return s;
 
-  p = (unsigned char *) dest;
+  p = (unsigned char *)dest;
   while ((p - (unsigned char *)dest) < dlen) {
     left = ((*p++) << 24);
     left |= ((*p++) << 16);
@@ -585,10 +571,10 @@ static char *decrypt_string_cbc(char *key, char *str)
 
     /* turn back into chars */
     for (i = 0; i < 32; i += 8) {
-        *--p = (unsigned char) ((right >> i) & 0xff);
+      *--p = (unsigned char)((right >> i) & 0xff);
     }
     for (i = 0; i < 32; i += 8) {
-        *--p = (unsigned char) ((left >> i) & 0xff);
+      *--p = (unsigned char)((left >> i) & 0xff);
     }
     p += 8;
   }
@@ -603,8 +589,7 @@ static char *decrypt_string_cbc(char *key, char *str)
 
 /* Returned string must be freed when done with it!
  */
-static char *decrypt_string(char *key, char *str)
-{
+static char *decrypt_string(char *key, char *str) {
   if (!egg_strncasecmp(key, "ecb:", 4)) {
     if (str[0] == '*') {
       /* ecb strings shouldn't start with * */
@@ -622,15 +607,14 @@ static char *decrypt_string(char *key, char *str)
     return decrypt_string_cbc(key + 4, str + 1);
 
   } else if (str[0] != '*') {
-      return decrypt_string_ecb(key, str);
+    return decrypt_string_ecb(key, str);
   }
 
   /* else */
   return decrypt_string_cbc(key, str + 1);
 }
 
-static int tcl_encrypt STDVAR
-{
+static int tcl_encrypt STDVAR {
   char *p;
 
   BADARGS(3, 3, " key string");
@@ -641,8 +625,7 @@ static int tcl_encrypt STDVAR
   return TCL_OK;
 }
 
-static int tcl_decrypt STDVAR
-{
+static int tcl_decrypt STDVAR {
   char *p;
 
   BADARGS(3, 3, " key string");
@@ -653,8 +636,7 @@ static int tcl_decrypt STDVAR
   return TCL_OK;
 }
 
-static int tcl_encpass STDVAR
-{
+static int tcl_encpass STDVAR {
   BADARGS(2, 2, " string");
 
   if (strlen(argv[1]) > 0) {
@@ -667,38 +649,29 @@ static int tcl_encpass STDVAR
   return TCL_OK;
 }
 
-static tcl_cmds mytcls[] = {
-  {"encrypt", tcl_encrypt},
-  {"decrypt", tcl_decrypt},
-  {"encpass", tcl_encpass},
-  {NULL,             NULL}
-};
+static tcl_cmds mytcls[] = {{"encrypt", tcl_encrypt},
+                            {"decrypt", tcl_decrypt},
+                            {"encpass", tcl_encpass},
+                            {NULL, NULL}};
 
-static tcl_strings my_tcl_strings[] = {
-  {"blowfish-use-mode", bf_mode, 3, 0},
-  {NULL,                NULL,    0, 0}
-};
+static tcl_strings my_tcl_strings[] = {{"blowfish-use-mode", bf_mode, 3, 0},
+                                       {NULL, NULL, 0, 0}};
 
-static char *blowfish_close()
-{
+static char *blowfish_close() {
   return "You can't unload the encryption module";
 }
 
 EXPORT_SCOPE char *blowfish_start(Function *);
 
 static Function blowfish_table[] = {
-  /* 0 - 3 */
-  (Function) blowfish_start,
-  (Function) blowfish_close,
-  (Function) blowfish_expmem,
-  (Function) blowfish_report,
-  /* 4 - 7 */
-  (Function) encrypt_string,
-  (Function) decrypt_string,
+    /* 0 - 3 */
+    (Function)blowfish_start, (Function)blowfish_close,
+    (Function)blowfish_expmem, (Function)blowfish_report,
+    /* 4 - 7 */
+    (Function)encrypt_string, (Function)decrypt_string,
 };
 
-char *blowfish_start(Function *global_funcs)
-{
+char *blowfish_start(Function *global_funcs) {
   int i;
 
   /* `global_funcs' is NULL if eggdrop is recovering from a restart.
@@ -723,9 +696,9 @@ char *blowfish_start(Function *global_funcs)
       module_undepend(MODULE_NAME);
       return "This module requires Eggdrop 1.8.0 or later.";
     }
-    add_hook(HOOK_ENCRYPT_PASS, (Function) blowfish_encrypt_pass);
-    add_hook(HOOK_ENCRYPT_STRING, (Function) encrypt_string);
-    add_hook(HOOK_DECRYPT_STRING, (Function) decrypt_string);
+    add_hook(HOOK_ENCRYPT_PASS, (Function)blowfish_encrypt_pass);
+    add_hook(HOOK_ENCRYPT_STRING, (Function)encrypt_string);
+    add_hook(HOOK_DECRYPT_STRING, (Function)decrypt_string);
   }
 
   /* ECB by default for now, change at v1.9.0! */
--- src/mod/blowfish.mod/blowfish.h
+++ src/mod/blowfish.mod/blowfish.h
@@ -23,26 +23,28 @@
 #ifndef _EGG_MOD_BLOWFISH_BLOWFISH_H
 #define _EGG_MOD_BLOWFISH_BLOWFISH_H
 
+#include "src/eggdrop.h"
+
 #define MAXKEYBYTES 56 /* 448 bits */
-#define bf_N        16
-#define noErr        0
-#define DATAERROR   -1
-#define KEYBYTES     8
+#define bf_N 16
+#define noErr 0
+#define DATAERROR -1
+#define KEYBYTES 8
 
 union aword {
   u_32bit_t word;
   u_8bit_t byte[4];
   struct {
 #ifdef WORDS_BIGENDIAN
-    unsigned int byte0:8;
-    unsigned int byte1:8;
-    unsigned int byte2:8;
-    unsigned int byte3:8;
-#else /* !WORDS_BIGENDIAN */
-    unsigned int byte3:8;
-    unsigned int byte2:8;
-    unsigned int byte1:8;
-    unsigned int byte0:8;
+    unsigned int byte0 : 8;
+    unsigned int byte1 : 8;
+    unsigned int byte2 : 8;
+    unsigned int byte3 : 8;
+#else  /* !WORDS_BIGENDIAN */
+    unsigned int byte3 : 8;
+    unsigned int byte2 : 8;
+    unsigned int byte1 : 8;
+    unsigned int byte0 : 8;
 #endif /* !WORDS_BIGENDIAN */
   } w;
 };
