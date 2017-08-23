1f99b333322a5fdbb2e0e0751361260ac710123f
Thomas Sader <thommey@gmail.com>
2017-08-23 19:17:27 +0200

    Add exit state to tls debug logs.

--- src/tls.c
+++ src/tls.c
@@ -643,10 +643,13 @@ void ssl_info(SSL *ssl, int where, int ret)
            (where & SSL_CB_READ) ? "read" : "write",
            SSL_alert_type_string_long(ret),
            SSL_alert_desc_string_long(ret));
+  } else if (where & SSL_CB_EXIT) {
+    putlog(data->loglevel, "*", "TLS: failed in: %s.",
+           SSL_state_string_long(ssl));
+  } else {
+    /* Display the state of the engine for debugging purposes */
+    debug1("TLS: state change: %s", SSL_state_string_long(ssl));
   }
-
-  /* Display the state of the engine for debugging purposes */
-  debug1("TLS: state change: %s", SSL_state_string_long(ssl));
 }
 
 /* Switch a socket to SSL communication
