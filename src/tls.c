8c53c1d785eebb2e15284bf05c0523e5bc4592ab
Thomas Sader <thommey@gmail.com>
2017-10-21 18:30:46 +0200

    Fix missing space in error msg.

--- src/tls.c
+++ src/tls.c
@@ -140,7 +140,7 @@ int ssl_init()
   ssl_files_loaded = 0;
   if (tls_certfile[0] ^ tls_keyfile[0]) {
     /* Both need to be set or unset */
-    putlog(LOG_MISC, "*", "ERROR: TLS: %s set but %s unset. Both must be set"
+    putlog(LOG_MISC, "*", "ERROR: TLS: %s set but %s unset. Both must be set "
 		   "to use a certificate, or unset both to disable.",
 		   tls_certfile[0] ? "ssl-certificate" : "ssl-privatekey",
 		   tls_certfile[0] ? "ssl-privatekey" : "ssl-certificate");
