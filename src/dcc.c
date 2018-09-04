77937e5a1e0ccdcd20479ecf2a2c8d0da2fe709e
michaelortmann <michael@zen>
2018-09-04 17:25:48 +0200

    doc

--- src/dcc.c
+++ src/dcc.c
@@ -67,7 +67,9 @@ int flood_telnet_thr = 5;       /* Number of telnet connections to be
 int flood_telnet_time = 60;     /* In how many seconds?                       */
 char network[41] = "unknown-net";      /* Name of the IRC network you're on   */
 char bannerfile[121] = "text/banner";  /* File displayed on telnet login      */
-char stealth_prompt[121] = "login: ";
+char stealth_prompt[121] = "login: ";  /* If stealth-telnets is 1, this
+                                        * setting will define the telnet
+                                        * prompt.                             */
 
 static void dcc_telnet_hostresolved(int);
 static void dcc_telnet_got_ident(int, char *);
