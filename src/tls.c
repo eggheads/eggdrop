833a6d1102e4284aaaf5027d9da554b25afa08a9
michaelortmann <michael@zen>
2018-09-05 03:22:09 +0200

    fix buffer and default

--- src/tls.c
+++ src/tls.c
@@ -43,8 +43,8 @@ char tls_capath[121] = "";    /* Path to trusted CA certificates              */
 char tls_cafile[121] = "";    /* File containing trusted CA certificates      */
 char tls_certfile[121] = "";  /* Our own digital certificate ;)               */
 char tls_keyfile[121] = "";   /* Private key for use with eggdrop             */
-char tls_protocols[121] = "TLSv1 TLSv1.1 TLSv1.2"; /* A list of protocols for SSL to use */
-char tls_ciphers[121] = "";   /* A list of ciphers for SSL to use             */
+char tls_protocols[121] = ""; /* A list of protocols for SSL to use */
+char tls_ciphers[141] = "";   /* A list of ciphers for SSL to use             */
 
 
 /* Count allocated memory for SSL. This excludes memory allocated by OpenSSL's
@@ -169,6 +169,8 @@ int ssl_init()
            ERR_error_string(ERR_get_error(), NULL));
     ERR_free_strings();
   }
+  printf("DEBUG: tls_protocols = >>>%s<<<\n", tls_protocols);
+  printf("DEBUG: tls_ciphers = >>>%s<<<\n", tls_ciphers);
   /* Let advanced users specify the list of allowed ssl protocols */
   #define EGG_SSLv2   1 << 0
   #define EGG_SSLv3   1 << 1
@@ -176,55 +178,57 @@ int ssl_init()
   #define EGG_TLSv1_1 1 << 3
   #define EGG_TLSv1_2 1 << 4
   #define EGG_TLSv1_3 1 << 5
-  char s[sizeof tls_protocols];
-  char *sep = " ";
-  char *word;
-  unsigned int protocols = 0;
-  strcpy(s, tls_protocols);
-  for (word = strtok(s, sep); word; word = strtok(NULL, sep)) {
-    if (!strcmp(word, "SSLv2"))
-      protocols |= EGG_SSLv2;
-    if (!strcmp(word, "SSLv3"))
-      protocols |= EGG_SSLv3;
-    if (!strcmp(word, "TLSv1"))
-      protocols |= EGG_TLSv1;
-    if (!strcmp(word, "TLSv1.1"))
-      protocols |= EGG_TLSv1_1;
-    if (!strcmp(word, "TLSv1.2"))
-      protocols |= EGG_TLSv1_2;
-    if (!strcmp(word, "TLSv1.3"))
-      protocols |= EGG_TLSv1_3;
-  }
-  if (!(protocols & EGG_SSLv2)) {
-    printf("DEBUG: set SSL_OP_NO_SSLv2\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv2);
-  }
-  if (!(protocols & EGG_SSLv3)) {
-    printf("DEBUG: set SSL_OP_NO_SSLv3\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv3);
-  }
-  if (!(protocols & EGG_TLSv1)) {
-    printf("DEBUG: set SSL_OP_NO_TLSv1\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1);
-  }
+  if (tls_protocols[0]) {
+    char s[sizeof tls_protocols];
+    char *sep = " ";
+    char *word;
+    unsigned int protocols = 0;
+    strcpy(s, tls_protocols);
+    for (word = strtok(s, sep); word; word = strtok(NULL, sep)) {
+      if (!strcmp(word, "SSLv2"))
+        protocols |= EGG_SSLv2;
+      if (!strcmp(word, "SSLv3"))
+        protocols |= EGG_SSLv3;
+      if (!strcmp(word, "TLSv1"))
+        protocols |= EGG_TLSv1;
+      if (!strcmp(word, "TLSv1.1"))
+        protocols |= EGG_TLSv1_1;
+      if (!strcmp(word, "TLSv1.2"))
+        protocols |= EGG_TLSv1_2;
+      if (!strcmp(word, "TLSv1.3"))
+        protocols |= EGG_TLSv1_3;
+    }
+    if (!(protocols & EGG_SSLv2)) {
+      printf("DEBUG: set SSL_OP_NO_SSLv2\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv2);
+    }
+    if (!(protocols & EGG_SSLv3)) {
+      printf("DEBUG: set SSL_OP_NO_SSLv3\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv3);
+    }
+    if (!(protocols & EGG_TLSv1)) {
+      printf("DEBUG: set SSL_OP_NO_TLSv1\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1);
+    }
 #ifdef SSL_OP_NO_TLSv1_1
-  if (!(protocols & EGG_TLSv1_1)) {
-    printf("DEBUG: set SSL_OP_NO_TLSv1_1\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_1);
-  }
+    if (!(protocols & EGG_TLSv1_1)) {
+      printf("DEBUG: set SSL_OP_NO_TLSv1_1\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_1);
+    }
 #endif
 #ifdef SSL_OP_NO_TLSv1_2
-  if (!(protocols & EGG_TLSv1_2)) {
-    printf("DEBUG: set SSL_OP_NO_TLSv1_2\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_2);
-  }
+    if (!(protocols & EGG_TLSv1_2)) {
+      printf("DEBUG: set SSL_OP_NO_TLSv1_2\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_2);
+    }
 #endif
 #ifdef SSL_OP_NO_TLSv1_3
-  if (!(protocols & EGG_TLSv1_3)) {
-    printf("DEBUG: set SSL_OP_NO_TLSv1_3\n");
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_3);
-  }
+    if (!(protocols & EGG_TLSv1_3)) {
+      printf("DEBUG: set SSL_OP_NO_TLSv1_3\n");
+      SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_3);
+    }
 #endif
+  }
   /* Let advanced users specify the list of allowed ssl ciphers */
   if (tls_ciphers[0] && !SSL_CTX_set_cipher_list(ssl_ctx, tls_ciphers)) {
     /* this replaces any preset ciphers so an invalid list is fatal */
