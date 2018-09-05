6ca0f55257feb17f6e9ff6f93fcfed8433856c46
michaelortmann <michael@zen>
2018-09-05 03:29:55 +0200

    fix buffer len, it was too long, what a waste

--- src/dcc.c
+++ src/dcc.c
@@ -67,7 +67,7 @@ int flood_telnet_thr = 5;       /* Number of telnet connections to be
 int flood_telnet_time = 60;     /* In how many seconds?                       */
 char network[41] = "unknown-net";      /* Name of the IRC network you're on   */
 char bannerfile[121] = "text/banner";  /* File displayed on telnet login      */
-char stealth_prompt[121] = "login: ";  /* If stealth-telnets is 1, this
+char stealth_prompt[81] = "login: ";   /* If stealth-telnets is 1, this
                                         * setting will define the telnet
                                         * prompt.                             */
 
