3f363c74dfe5cabd2c43c9268599c972c97f0522
michaelortmann <michael@zen>
2018-09-17 22:54:29 +0200

    fix if statement

--- src/tclhash.c
+++ src/tclhash.c
@@ -746,12 +746,13 @@ static int trigger_bind(const char *proc, const char *param,
   x = Tcl_VarEval(interp, proc, param, NULL);
   Context;
   if (proc && proc[0] != '*' && !r) {
-    if (!getrusage(RUSAGE_SELF, &ru2));
-    debug3("triggered bind %s, user %.3fms sys %.3fms", proc,
-           (double) (ru2.ru_utime.tv_usec - ru1.ru_utime.tv_usec) / 1000 +
-           (double) (ru2.ru_utime.tv_sec  - ru1.ru_utime.tv_sec ) * 1000,
-           (double) (ru2.ru_stime.tv_usec - ru1.ru_stime.tv_usec) / 1000 +
-           (double) (ru2.ru_stime.tv_sec  - ru1.ru_stime.tv_sec ) * 1000);
+    if (!getrusage(RUSAGE_SELF, &ru2)) {
+      debug3("triggered bind %s, user %.3fms sys %.3fms", proc,
+             (double) (ru2.ru_utime.tv_usec - ru1.ru_utime.tv_usec) / 1000 +
+             (double) (ru2.ru_utime.tv_sec  - ru1.ru_utime.tv_sec ) * 1000,
+             (double) (ru2.ru_stime.tv_usec - ru1.ru_stime.tv_usec) / 1000 +
+             (double) (ru2.ru_stime.tv_sec  - ru1.ru_stime.tv_sec ) * 1000);
+    }
   }
 
   if (x == TCL_ERROR) {
