cc6664683cc9d8b1ec0ecc6f6d9ad8d8af2ffd60
michaelortmann <michael@zen>
2018-09-26 02:11:54 +0200

    Fix buffer size - fix a gcc 8 format-truncation warning

--- src/dcc.c
+++ src/dcc.c
@@ -1470,7 +1470,7 @@ static void dcc_dupwait(int idx, char *buf, int i)
  */
 static void timeout_dupwait(int idx)
 {
-  char x[100];
+  char x[NICKLEN + UHOSTLEN];
 
   /* Still duplicate? */
   if (in_chain(dcc[idx].nick)) {
