4f4c14dd85a8eb92f080565fefbde4db6a2d65dd
michaelortmann <michael@zen>
2018-10-01 00:58:45 +0200

    revert formatting to keep the patch simple

--- src/mod/dns.mod/coredns.c
+++ src/mod/dns.mod/coredns.c
@@ -1274,13 +1274,13 @@ static int init_dns_core(void)
 
   /* Initialise the resolv library. */
   res_init();
-#ifdef NETBSD_HACKS
-  puts("netbsd found. if eggdrop crashes with\n"
-       "  _res is not supported for multi-threaded programs.\n"
-       "  [1]   Abort trap (core dumped) ./eggdrop -nt\n"
-       "dns.mod must be disabled by commenting out in eggdrop.conf\n"
-       "  #loadmodule dns");
-#endif
+  #ifdef NETBSD_HACKS
+    puts("netbsd found. if eggdrop crashes with\n"
+         "  _res is not supported for multi-threaded programs.\n"
+         "  [1]   Abort trap (core dumped) ./eggdrop -nt\n"
+         "dns.mod must be disabled by commenting out in eggdrop.conf\n"
+         "  #loadmodule dns");
+  #endif
   _res.options |= RES_RECURSE | RES_DEFNAMES | RES_DNSRCH;
   for (i = 0; i < _res.nscount; i++)
     _res.nsaddr_list[i].sin_family = AF_INET;
