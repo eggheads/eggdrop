6e1a6e25bd94382519078669cbc9b05fd9c91ab5
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2018-11-25 01:33:20 +0100

    More strcpy() -> strlcpy()
    Found by: thommey, michaelortmann
    Patch by: michaelortmann

--- src/mod/server.mod/servmsg.c
+++ src/mod/server.mod/servmsg.c
@@ -491,7 +491,7 @@ static int gotmsg(char *from, char *msg)
   ignoring = match_ignore(from);
   to = newsplit(&msg);
   fixcolon(msg);
-  strlcpy(uhost, from, sizeof(buf));
+  strlcpy(uhost, from, sizeof buf);
   nick = splitnick(&uhost);
   /* Apparently servers can send CTCPs now too, not just nicks */
   if (nick[0] == '\0')
@@ -626,7 +626,7 @@ static int gotnotice(char *from, char *msg)
   ignoring = match_ignore(from);
   to = newsplit(&msg);
   fixcolon(msg);
-  strcpy(uhost, from);
+  strlcpy(uhost, from, sizeof buf);
   nick = splitnick(&uhost);
 
   /* Check for CTCP: */
