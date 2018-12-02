cf5e32ee3a0e790ae9fb90b60495e791da9e51b8
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2018-12-02 03:30:29 +0100

    Streamline return
    Patch by: michaelortmann

--- src/mod/server.mod/servmsg.c
+++ src/mod/server.mod/servmsg.c
@@ -278,9 +278,7 @@ static int check_tcl_out(int which, char *msg, int sent)
 
 static int match_my_nick(char *nick)
 {
-  if (!rfc_casecmp(nick, botname))
-    return 1;
-  return 0;
+  return (!rfc_casecmp(nick, botname));
 }
 
 /* 001: welcome to IRC (use it to fix the server name) */
