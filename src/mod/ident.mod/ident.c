379f2d645fdb756d62ea994f411a2356c7057a6a
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2019-06-27 01:48:53 +0200

    Update copyright. Happy 2019!

--- src/mod/ident.mod/ident.c
+++ src/mod/ident.mod/ident.c
@@ -4,7 +4,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2018 Michael Ortmann
+ * Copyright (c) 2018 - 2019 Michael Ortmann
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
