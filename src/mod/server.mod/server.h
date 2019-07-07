285bd532c9fe9ed6c3243c0e52b18fb4939b9e8a
Geo <geo@eggheads.org>
2019-07-07 13:13:07 -0500

    Move define from eggdrop to server

--- src/mod/server.mod/server.h
+++ src/mod/server.mod/server.h
@@ -20,6 +20,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#define CAPMAX      511           /* CAP list string max                 */
+
 #ifndef _EGG_MOD_SERVER_SERVER_H
 #define _EGG_MOD_SERVER_SERVER_H
 
