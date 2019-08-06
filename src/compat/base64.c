51dea390bb0cf43ba0a79702052f763b6b655fda
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2019-08-07 01:54:48 +0200

    Copyright / cleanup

--- src/compat/base64.c
+++ src/compat/base64.c
@@ -1,4 +1,28 @@
 /*
+ * base64.c -- provides b64_ntop() and b64_pton() if necessary
+ */
+/*
+ * Copyright (C) 2010 - 2019 Eggheads Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include <config.h>
+
+#ifndef HAVE_STRLCPY
+/*
  * Copyright (c) 1996, 1998 by Internet Software Consortium.
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -314,3 +338,4 @@ b64_pton(const char *src, u_char *target, size_t targsize)
 
 	return (tarindex);
 }
+#endif /* HAVE_STRLCPY */
