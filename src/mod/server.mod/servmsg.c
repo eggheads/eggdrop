08b824dedb2813733bf623eed32f881fa3407869
Geo <vanosg@users.noreply.github.com>
2019-11-25 22:54:48 -0600

    Update servmsg.c

--- src/mod/server.mod/servmsg.c
+++ src/mod/server.mod/servmsg.c
@@ -1253,7 +1253,7 @@ static int tryauthenticate(char *from, char *msg)
 
 static int gotauthenticate(char *from, char *msg)
 {
-  fixcolon(msg);
+  fixcolon(msg); /* Because Inspircd does its own thing */
   if (tryauthenticate(from, msg) && !sasl_continue) {
     putlog(LOG_DEBUG, "*", "SASL: Aborting connection and retrying");
     nuke_server("Quitting...");
