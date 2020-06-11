30d13c0747cc1f8fdabe0ab855efb91625b42319
Geo <geo@eggheads.org>
2020-06-11 14:24:13 -0500

    insert comment here

--- src/mod/irc.mod/chan.c
+++ src/mod/irc.mod/chan.c
@@ -1710,7 +1710,9 @@ static int gotjoin(char *from, char *channame)
   strlcpy(uhost, from, sizeof buf);
   nick = splitnick(&uhost);
   chname = newsplit(&channame);
-  fixcolon(chname);
+  if (!extended_join) {
+    fixcolon(chname);
+  }
   chan = findchan_by_dname(chname);
   if (!chan && chname[0] == '!') {
     /* As this is a !channel, we need to search for it by display (short)
