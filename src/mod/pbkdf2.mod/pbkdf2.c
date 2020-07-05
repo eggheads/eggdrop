ddd4c4a1451d8fe4a58f39325f3db407a894cc78
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2020-07-05 20:57:09 +0200

    Fix again

--- src/mod/pbkdf2.mod/pbkdf2.c
+++ src/mod/pbkdf2.mod/pbkdf2.c
@@ -94,8 +94,8 @@ static char *pbkdf2_hash(const char *pass, const char *digest_name,
   digestlen = EVP_MD_size(digest);
   outlen = strlen("$pbkdf2-") + strlen(digest_name) +
            strlen("$rounds=4294967295$i") + B64_NTOP_CALCULATE_SIZE(saltlen) +
-           1 + B64_NTOP_CALCULATE_SIZE(digestlen) + 1;
-  if (outlen > sizeof out) {
+           1 + B64_NTOP_CALCULATE_SIZE(digestlen);
+  if ((outlen + 1) > sizeof out) {
     putlog(LOG_MISC, "*", "PBKDF2 error: outlen %i > sizeof out %i.", outlen,
            sizeof out);
     return NULL;
