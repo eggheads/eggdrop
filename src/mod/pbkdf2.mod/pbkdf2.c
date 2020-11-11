beb7b4fb9f97e664874665e45fac056e2a897199
Geo <geo@eggheads.org>
2020-11-11 00:40:52 -0500

    more docs

--- src/mod/pbkdf2.mod/pbkdf2.c
+++ src/mod/pbkdf2.mod/pbkdf2.c
@@ -27,7 +27,7 @@ static char pbkdf2_method[28] = "SHA256";
 /* Enable re-encoding of password if pbkdf2-method and / or pbkdf2-rounds
  * change.
  */
-static int pbkdf2_re_encode = 0;
+static int pbkdf2_re_encode = 1;
 /* Number of rounds (iterations). Default is 5000 like in glibc. */
 static int pbkdf2_rounds = 16000;
 
