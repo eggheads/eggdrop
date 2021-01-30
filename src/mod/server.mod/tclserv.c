be06de8f58643e8abce10c8bd185ab96506c5233
Geo <geo@eggheads.org>
2021-01-29 22:54:24 -0500

    Fix add/delserver syntax typo Found by: ZarTek

--- src/mod/server.mod/tclserv.c
+++ src/mod/server.mod/tclserv.c
@@ -450,7 +450,7 @@ static int tcl_addserver STDVAR {
   char pass[121] = "";
   char ret = 0;
 
-  BADARGS(2, 4, "server ?port? ?pass?");
+  BADARGS(2, 4, " server ?port? ?pass?");
   strlcpy(name, argv[1], sizeof name);
   if (argc >= 3) {
       strlcpy(port, argv[2], sizeof port);
@@ -477,7 +477,7 @@ static int tcl_delserver STDVAR {
   char port[7] = "";
   char ret = 0;
 
-  BADARGS(2, 3, "server, ?port?");
+  BADARGS(2, 3, " server, ?port?");
   strlcpy(name, argv[1], sizeof name);
   if (argc == 3) {
     strlcpy(port, argv[2], sizeof port);
