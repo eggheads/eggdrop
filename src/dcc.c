53ad182b2fafd64f637f15d88aefa8277824649d
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2021-03-18 01:06:31 +0100

    Do not send escape codes to ssl connections for openssl does not interpret but print them

--- src/dcc.c
+++ src/dcc.c
@@ -2344,7 +2344,8 @@ static void dcc_telnet_got_ident(int i, char *host)
   /* Note: we don't really care about telnet status here. We use the
    * STATUS option as a hopefully harmless way to detect if the other
    * side is a telnet client or not. */
-  dprintf(i, TLN_IAC_C TLN_WILL_C TLN_STATUS_C);
+  if (!dcc[i].ssl)
+    dprintf(i, TLN_IAC_C TLN_WILL_C TLN_STATUS_C);
 
   /* Copy acceptable-nick/host mask */
   dcc[i].status = STAT_TELNET | STAT_ECHO;
