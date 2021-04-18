4384b3810ee538e33168452d560e20e912d6320e
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2021-04-18 22:54:18 +0200

    Fix strcpy-param-overlap

--- src/mod/filesys.mod/files.c
+++ src/mod/filesys.mod/files.c
@@ -162,7 +162,7 @@ static int resolve_dir(char *current, char *change, char **real, int idx)
   if (new[0] == '/') {
     /* EVERYONE has access here */
     (*real)[0] = 0;
-    strcpy(new, &new[1]);
+    memmove(new, new + 1, strlen(new));
   }
   /* Cycle thru the elements */
   strcat(new, "/");
