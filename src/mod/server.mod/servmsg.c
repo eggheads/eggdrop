36a23823e58c4fd73b18083820047aadda4bb146
Geo <geo@eggheads.org>
2021-11-27 10:53:15 -0500

    src/mod/server.mod/servmsg.c

--- src/mod/server.mod/servmsg.c
+++ src/mod/server.mod/servmsg.c
@@ -878,7 +878,7 @@ static int got432(char *from, char *msg)
   else {
     putlog(LOG_MISC, "*", IRC_BADBOTNICK);
     if (!strcmp(erroneous, origbotname)) {
-      strlcpy(nick, altnick, sizeof nick);
+      strlcpy(nick, get_altbotnick(), sizeof nick);
     } else {
       make_rand_str_from_chars(nick, sizeof nick - 1, CHARSET_LOWER_ALPHA);
     }
