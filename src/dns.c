eggheads/eggdrop

commit 247c6a58e43399a05a7a6db94a35b3196a816279

prevent race in threaded dns
Found by: michaelortmann
Patch by: michaelortmann

src/dns.c
@@ -543,8 +543,10 @@ void *thread_dns_ipbyhost(void *arg)
 #endif
     freeaddrinfo(res0);
   }
+  pthread_mutex_lock(&dtn->mutex);
   dtn->ok = !i;
   close(dtn->fildes[1]);
+  pthread_mutex_unlock(&dtn->mutex);
   return NULL;
 }
 

src/eggdrop.h
@@ -788,6 +788,7 @@ enum {
 
 /* linked list instead of array because of multi threading */
 struct dns_thread_node {
+  pthread_mutex_t mutex;
   int fildes[2];
   int type;
   sockname_t addr;

src/net.c
@@ -1034,10 +1034,16 @@ int sockread(char *s, int *len, sock_list *slist, int slistmax, int tclonly)
   for (dtn = dtn_prev->next; dtn; dtn = dtn->next) {
     fd = dtn->fildes[0];
     if (FD_ISSET(fd, &fdr)) {
-      if (dtn->type == DTN_TYPE_HOSTBYIP)
+      if (dtn->type == DTN_TYPE_HOSTBYIP) {
+        pthread_mutex_lock(&dtn->mutex);
         call_hostbyip(&dtn->addr, dtn->host, dtn->ok);
-      else
+        pthread_mutex_unlock(&dtn->mutex);
+      }
+      else {
+        pthread_mutex_lock(&dtn->mutex);
         call_ipbyhost(dtn->host, &dtn->addr, dtn->ok);
+        pthread_mutex_unlock(&dtn->mutex);
+      }
       close(dtn->fildes[0]);
       dtn_prev->next = dtn->next;
       nfree(dtn);

commit 0f4a39ac5bf20e55ff300cb2b4d1b633dde36b7a

Fix tdns mutex
Found by: Geo
Patch by: michaelortmann

Fixes a mutex issue introduced in #1237

src/dns.c
@@ -504,8 +504,10 @@ void *thread_dns_hostbyip(void *arg)
 #endif
       inet_ntop(AF_INET, &addr->addr.s4.sin_addr.s_addr, dtn->host, sizeof dtn->host);
   }
+  pthread_mutex_lock(&dtn->mutex);
   dtn->ok = !i;
   close(dtn->fildes[1]);
+  pthread_mutex_unlock(&dtn->mutex);
   return NULL;
 }
 
@@ -555,6 +557,8 @@ void core_dns_hostbyip(sockname_t *addr)
   struct dns_thread_node *dtn = nmalloc(sizeof(struct dns_thread_node));
   pthread_t thread; /* only used by pthread_create(), no need to save */
 
+  if (pthread_mutex_init(&dtn->mutex, NULL))
+    fatal("ERROR: core_dns_hostbyip(): pthread_mutex_init() failed", 0);
   if (pipe(dtn->fildes) < 0) {
     putlog(LOG_MISC, "*", "core_dns_hostbyip(): pipe(): error: %s", strerror(errno));
     call_hostbyip(addr, iptostr(&addr->addr.sa), 0);
@@ -587,6 +591,8 @@ void core_dns_ipbyhost(char *host)
     return;
   }
   dtn = nmalloc(sizeof(struct dns_thread_node));
+  if (pthread_mutex_init(&dtn->mutex, NULL))
+    fatal("ERROR: core_dns_ipbyhost(): pthread_mutex_init() failed", 0);
   if (pipe(dtn->fildes) < 0) {
     putlog(LOG_MISC, "*", "core_dns_ipbyhost(): pipe(): error: %s", strerror(errno));
     call_ipbyhost(host, &addr, 0);