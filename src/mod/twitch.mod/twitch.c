a18e14c3d3fbd5a56ffe99440721731f9e33b749
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2022-02-26 23:12:55 +0000

    Fix twitch restart/rehash crash
    Found by: roughnecks
    Patch by: Lord255, michaelortmann, thommey
    Fixes: #1263, #1196
    
    keep-nick was set to STR_PROTECT (read-only) with the intention of forcing it to be 0 because ISON is not supported on twitch, but keep-nick 1 generates those.
    
    (cherry picked from commit 2b4b0eb2ecd6dc2b27e4d54e49702d04dfef09b9)

--- src/mod/twitch.mod/twitch.c
+++ src/mod/twitch.mod/twitch.c
@@ -50,7 +50,6 @@ static Function *global = NULL, *server_funcs = NULL;
 static p_tcl_bind_list H_ccht, H_cmsg, H_htgt, H_wspr, H_wspm, H_rmst, H_usst, H_usrntc;
 
 twitchchan_t *twitchchan = NULL;
-static int keepnick;
 static char cap_request[55];
 
 /* Calculate the memory we keep allocated.
@@ -82,6 +81,22 @@ void remove_chars(char* str, char c) {
         pw += (*pw != c);
     }
     *pw = '\0';
+}
+
+char *traced_keepnick(ClientData cd, Tcl_Interp *irp, EGG_CONST char *name1,
+                   EGG_CONST char *name2, int flags)
+{
+  const char *value;
+
+  if (flags & TCL_TRACE_DESTROYED) {
+    Tcl_TraceVar(interp, "keep-nick", TCL_GLOBAL_ONLY|TCL_TRACE_WRITES|TCL_TRACE_UNSETS, traced_keepnick, NULL);
+  }
+  value = Tcl_GetVar2(irp, name1, name2, TCL_GLOBAL_ONLY);
+  if (value && strcmp(value, "0")) {
+    putlog(LOG_MISC, "*", "Twitch: keep-nick is forced to be 0 when twitch.mod is loaded");
+    Tcl_SetVar2(irp, name1, name2, "0", TCL_GLOBAL_ONLY);
+  }
+  return NULL;
 }
 
 static void cmd_twcmd(struct userrec *u, int idx, char *par) {
@@ -792,10 +807,11 @@ static tcl_cmds mytcl[] = {
   {NULL,                   NULL}
 };
 
+/*
 static tcl_ints my_tcl_ints[] = {
-  {"keep-nick",         &keepnick,        STR_PROTECT},
   {NULL,                NULL,                       0}
 };
+*/
 
 static tcl_strings my_tcl_strings[] = {
   {"cap-request",   cap_request,    55,     STR_PROTECT},
@@ -823,11 +839,13 @@ static cmd_t twitch_rawt[] = {
 static char *twitch_close()
 {
   Context;
+  Tcl_UntraceVar(interp, "keep-nick", TCL_GLOBAL_ONLY|TCL_TRACE_WRITES|TCL_TRACE_UNSETS, traced_keepnick, NULL);
   rem_builtins(H_dcc, mydcc);
   rem_builtins(H_raw, twitch_raw);
   rem_builtins(H_rawt, twitch_rawt);
   rem_tcl_commands(mytcl);
-  rem_tcl_ints(my_tcl_ints);
+  // rem_tcl_ints(my_tcl_ints);
+  rem_tcl_strings(my_tcl_strings);
   del_bind_table(H_ccht);
   del_bind_table(H_cmsg);
   del_bind_table(H_htgt);
@@ -859,11 +877,12 @@ static Function twitch_table[] = {
 
 char *twitch_start(Function *global_funcs)
 {
+  const char *value;
+
   /* Assign the core function table. After this point you use all normal
    * functions defined in src/mod/modules.h
    */
   global = global_funcs;
-  keepnick = 0;
 
   Context;
   /* Register the module. */
@@ -894,7 +913,7 @@ char *twitch_start(Function *global_funcs)
  */
   if (net_type_int != NETT_TWITCH) {
     fatal("ERROR: ATTEMPTED TO LOAD TWITCH MODULE WITH INCORRECT NET-TYPE SET\n"
-          "  Please check net-type in config and try again", 0);
+          "  Please check that net-type is set to twitch in config before loadmodule twitch and try again", 0);
   }
 
   H_ccht = add_bind_table("ccht", HT_STACKABLE, twitch_2char);
@@ -906,17 +925,22 @@ char *twitch_start(Function *global_funcs)
   H_usst = add_bind_table("usst", HT_STACKABLE, twitch_3char);
   H_usrntc = add_bind_table("usrntc", HT_STACKABLE, twitch_3char);
 
-/* Override config setting with these values; they are required for Twitch */
+  /* Override config setting with these values; they are required for Twitch */
   Tcl_SetVar(interp, "cap-request",
         "twitch.tv/commands twitch.tv/membership twitch.tv/tags", 0);
-  Tcl_SetVar(interp, "keep-nick", "0", 0);  /* keep-nick causes ISONs to be
-                                             * sent, which are not supported */
+  /* keep-nick causes ISONs to be sent, which are not supported */
+  if ((value = Tcl_GetVar2(interp, "keep-nick", NULL, TCL_GLOBAL_ONLY)) && strcmp(value, "0")) {
+    putlog(LOG_MISC, "*", "Twitch: keep-nick is forced to be 0 when twitch.mod is loaded");
+  }
+  Tcl_SetVar2(interp, "keep-nick", NULL, "0", TCL_GLOBAL_ONLY);
+  Tcl_TraceVar(interp, "keep-nick", TCL_GLOBAL_ONLY|TCL_TRACE_WRITES|TCL_TRACE_UNSETS, traced_keepnick, NULL);
+
   /* Add command table to bind list */
   add_builtins(H_dcc, mydcc);
   add_builtins(H_raw, twitch_raw);
   add_builtins(H_rawt, twitch_rawt);
   add_tcl_commands(mytcl);
-  add_tcl_ints(my_tcl_ints);
+  // add_tcl_ints(my_tcl_ints);
   add_tcl_strings(my_tcl_strings);
   return NULL;
 }
