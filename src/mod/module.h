eggheads/eggdrop

commit faec90001ab5f8795b1d9d85f2ddfc31fec50eea

Fix format specifiers
Patch by: thommey
Fixes: #1176

Needs to be tested and compile tested on various platforms if possible.
Uses difftime() to print time_t when possible because printing time_t portably is very hard.

* Add compiler format specifier checking where possible
* Fix various printf-style specifiers and arguments
* add format specifiers

Co-authored-by: Geo <geo@eggheads.org>

src/mod/module.h
@@ -80,6 +80,12 @@
 #define EGG_IS_MIN_VER(ver)             ((ver) <= EGG_VERSION)
 #define EGG_IS_MAX_VER(ver)             ((ver) >= EGG_VERSION)
 
+typedef void (*putlogfunc)(int, char*, const char*, ...) ATTRIBUTE_FORMAT(printf,3,4);
+typedef void (*shareoutfunc)(struct chanset_t*, const char*, ...) ATTRIBUTE_FORMAT(printf,2,3);
+typedef void (*dprintffunc)(int, const char *, ...) ATTRIBUTE_FORMAT(printf,2,3);
+typedef void (*chatoutfunc)(const char *, ...) ATTRIBUTE_FORMAT(printf,1,2);
+typedef void (*chanout_butfunc)(int, int, const char *, ...) ATTRIBUTE_FORMAT(printf,3,4);
+
 /* Redefine for module-relevance */
 
 /* 0 - 3 */
@@ -172,10 +178,10 @@
 #define flagrec_eq ((int(*)(struct flag_record*,struct flag_record *))global[66])
 #define flagrec_ok ((int(*)(struct flag_record*,struct flag_record *))global[67])
 /* 68 - 71 */
-#define shareout (*(Function *)(global[68]))
-#define dprintf (global[69])
-#define chatout (global[70])
-#define chanout_but ((void(*)())global[71])
+#define shareout ((shareoutfunc)(global[68]))
+#define dprintf ((dprintffunc)(global[69]))
+#define chatout ((chatoutfunc)(global[70]))
+#define chanout_but ((chanout_butfunc)(global[71]))
 /* 72 - 75 */
 #define check_validity ((int (*) (char *,IntFunc))global[72])
 #define egg_list_delete ((int (*)( struct list_type **, struct list_type *))global[73])
@@ -347,7 +353,7 @@
 #define USERENTRY_COMMENT (*(struct user_entry_type *)(global[195]))
 /* 196 - 199 */
 #define USERENTRY_LASTON (*(struct user_entry_type *)(global[196]))
-#define putlog (global[197])
+#define putlog ((putlogfunc)(global[197]))
 #define botnet_send_chan ((void(*)(int,char*,char*,int,char*))global[198])
 #define list_type_kill ((void(*)(struct list_type *))global[199])
 /* 200 - 203 */

src/botcmd.c
@@ -1130,7 +1130,7 @@ static void bot_filereq(int idx, char *tobot)
       module_entry *fs = module_find("filesys", 0, 0);
 
       if (fs == NULL)
-        botnet_send_priv(idx, botnetnick, from, NULL, MOD_NOFILESYSMOD);
+        botnet_send_priv(idx, botnetnick, from, NULL, "%s", MOD_NOFILESYSMOD);
       else {
         Function f = fs->funcs[FILESYS_REMOTE_REQ];
 

src/botmsg.c
@@ -40,15 +40,14 @@ static char OBUF[1024];
 #ifndef NO_OLD_BOTNET
 /* Ditto for tandem bots
  */
-void tandout_but EGG_VARARGS_DEF(int, arg1)
+ATTRIBUTE_FORMAT(printf,2,3)
+void tandout_but(int x, const char *format, ...)
 {
-  int i, x, len;
-  char *format;
+  int i, len;
   char s[511];
   va_list va;
 
-  x = EGG_VARARGS_START(int, arg1, va);
-  format = va_arg(va, char *);
+  va_start(va, format);
 
   len = egg_vsnprintf(s, sizeof s, format, va);
   va_end(va);
@@ -136,14 +135,16 @@ char *unsigned_int_to_base10(unsigned int val)
   return buf_base10 + i;
 }
 
-int simple_sprintf EGG_VARARGS_DEF(char *, arg1)
+// TODO: this should probably not be used eggdrop core anymore
+// and only stay for 3rd party module compatibility
+// Reason: No sane compiler error checking possible, hardcoded 1024 limit
+int simple_sprintf (char *buf, const char *format, ...)
 {
-  char *buf, *format, *s;
+  char *s;
   int c = 0, i;
   va_list va;
 
-  buf = EGG_VARARGS_START(char *, arg1, va);
-  format = va_arg(va, char *);
+  va_start(va, format);
 
   while (*format && c < 1023) {
     if (*format == '%') {
@@ -289,19 +290,14 @@ void botnet_send_pong(int idx)
     dprintf(idx, "po\n");
 }
 
-void botnet_send_priv EGG_VARARGS_DEF(int, arg1)
+ATTRIBUTE_FORMAT(printf,5,6)
+void botnet_send_priv (int idx, char *from, char *to, char *tobot, const char *format, ...)
 {
-  int idx, l;
-  char *from, *to, *tobot, *format;
+  int l;
   char tbuf[1024];
   va_list va;
 
-  idx = EGG_VARARGS_START(int, arg1, va);
-  from = va_arg(va, char *);
-  to = va_arg(va, char *);
-  tobot = va_arg(va, char *);
-  format = va_arg(va, char *);
-
+  va_start(va, format);
   egg_vsnprintf(tbuf, 450, format, va);
   va_end(va);
   tbuf[sizeof(tbuf) - 1] = 0;
@@ -616,7 +612,7 @@ void botnet_send_join_idx(int useridx, int oldchan)
                        dcc[useridx].sock, dcc[useridx].host);
     send_tand_but(-1, OBUF, -l);
 #ifndef NO_OLD_BOTNET
-    tandout_but(-1, "join %s %s %d %c%d %s\n", botnetnick,
+    tandout_but(-1, "join %s %s %d %c%ld %s\n", botnetnick,
                 dcc[useridx].nick, dcc[useridx].u.chat->channel,
                 geticon(useridx), dcc[useridx].sock, dcc[useridx].host);
     tandout_but(-1, "chan %s %d %s %s %s.\n",
@@ -674,7 +670,7 @@ void botnet_send_part_idx(int useridx, char *reason)
   if (tands > 0) {
     send_tand_but(-1, OBUF, -l);
 #ifndef NO_OLD_BOTNET
-    tandout_but(-1, "part %s %s %d\n", botnetnick,
+    tandout_but(-1, "part %s %s %ld\n", botnetnick,
                 dcc[useridx].nick, dcc[useridx].sock);
     tandout_but(-1, "chan %s %d %s has left the %s%s%s.\n",
                 botnetnick, dcc[useridx].u.chat->channel,
@@ -718,9 +714,9 @@ void botnet_send_nkch(int useridx, char *oldnick)
                        dcc[useridx].sock, dcc[useridx].nick);
     send_tand_but(-1, OBUF, -l);
 #ifndef NO_OLD_BOTNET
-    tandout_but(-1, "part %s %s %d\n", botnetnick,
+    tandout_but(-1, "part %s %s %ld\n", botnetnick,
                 dcc[useridx].nick, dcc[useridx].sock);
-    tandout_but(-1, "join %s %s %d %c%d %s\n", botnetnick,
+    tandout_but(-1, "join %s %s %d %c%ld %s\n", botnetnick,
                 dcc[useridx].nick, dcc[useridx].u.chat->channel,
                 geticon(useridx), dcc[useridx].sock, dcc[useridx].host);
     tandout_but(-1, "chan %s %d %s: %s -> %s.\n",
@@ -798,7 +794,7 @@ int add_note(char *to, char *from, char *msg, int idx, int echo)
     i = nextbot(p);
     if (i < 0) {
       if (idx >= 0)
-        dprintf(idx, BOT_NOTHERE);
+        dprintf(idx, "%s", BOT_NOTHERE);
 
       return NOTE_ERROR;
     }
@@ -839,7 +835,7 @@ int add_note(char *to, char *from, char *msg, int idx, int echo)
   u = get_user_by_handle(userlist, to);
   if (!u) {
     if (idx >= 0)
-      dprintf(idx, USERF_UNKNOWN);
+      dprintf(idx, "%s", USERF_UNKNOWN);
 
     return NOTE_ERROR;
   }
@@ -847,7 +843,7 @@ int add_note(char *to, char *from, char *msg, int idx, int echo)
   /* Is the note to a bot? */
   if (is_bot(u)) {
     if (idx >= 0)
-      dprintf(idx, BOT_NONOTES);
+      dprintf(idx, "%s", BOT_NONOTES);
 
     return NOTE_ERROR;
   }

src/botnet.c
@@ -1135,7 +1135,7 @@ static void failed_tandem_relay(int idx)
         (dcc[i].u.relay->sock == dcc[idx].sock))
       uidx = i;
   if (uidx < 0) {
-    putlog(LOG_MISC, "*", "%s  %d -> %d", BOT_CANTFINDRELAYUSER,
+    putlog(LOG_MISC, "*", "%s  %ld -> %d", BOT_CANTFINDRELAYUSER,
            dcc[idx].sock, dcc[idx].u.relay->sock);
     killsock(dcc[idx].sock);
     lostdcc(idx);
@@ -1258,7 +1258,7 @@ static void tandem_relay_resolve_failure(int idx)
       break;
     }
   if (uidx < 0) {
-    putlog(LOG_MISC, "*", "%s  %d -> %d", BOT_CANTFINDRELAYUSER,
+    putlog(LOG_MISC, "*", "%s  %ld -> %d", BOT_CANTFINDRELAYUSER,
            dcc[idx].sock, dcc[idx].u.relay->sock);
     killsock(dcc[idx].sock);
     lostdcc(idx);
@@ -1331,7 +1331,7 @@ static void pre_relay(int idx, char *buf, int i)
       }
   }
   if (tidx < 0) {
-    putlog(LOG_MISC, "*", "%s  %d -> %d", BOT_CANTFINDRELAYUSER,
+    putlog(LOG_MISC, "*", "%s  %ld -> %d", BOT_CANTFINDRELAYUSER,
            dcc[idx].sock, dcc[idx].u.relay->sock);
     killsock(dcc[idx].sock);
     lostdcc(idx);
@@ -1377,7 +1377,7 @@ static void failed_pre_relay(int idx)
       }
   }
   if (tidx < 0) {
-    putlog(LOG_MISC, "*", "%s  %d -> %d", BOT_CANTFINDRELAYUSER,
+    putlog(LOG_MISC, "*", "%s  %ld -> %d", BOT_CANTFINDRELAYUSER,
            dcc[idx].sock, dcc[idx].u.relay->sock);
     killsock(dcc[idx].sock);
     lostdcc(idx);
@@ -1411,7 +1411,7 @@ static void cont_tandem_relay(int idx, char *buf, int i)
         (dcc[i].u.relay->sock == dcc[idx].sock))
       uidx = i;
   if (uidx < 0) {
-    putlog(LOG_MISC, "*", "%s  %d -> %d", BOT_CANTFINDRELAYUSER,
+    putlog(LOG_MISC, "*", "%s  %ld -> %d", BOT_CANTFINDRELAYUSER,
            dcc[i].sock, dcc[i].u.relay->sock);
     killsock(dcc[i].sock);
     lostdcc(i);

src/chanprog.c
@@ -511,7 +511,7 @@ void chanprog()
 void reload()
 {
   if (!file_readable(userfile)) {
-    putlog(LOG_MISC, "*", MISC_CANTRELOADUSER);
+    putlog(LOG_MISC, "*", "%s", MISC_CANTRELOADUSER);
     return;
   }
 

src/cmds.c
@@ -2848,7 +2848,7 @@ static void cmd_tcl(struct userrec *u, int idx, char *msg)
   Tcl_DString dstr;
 
   if (!(isowner(dcc[idx].nick)) && (must_be_owner)) {
-    dprintf(idx, MISC_NOSUCHCMD);
+    dprintf(idx, "%s", MISC_NOSUCHCMD);
     return;
   }
   debug1("tcl: evaluate (.tcl): %s", msg);
@@ -2876,7 +2876,7 @@ static void cmd_set(struct userrec *u, int idx, char *msg)
   Tcl_DString dstr;
 
   if (!(isowner(dcc[idx].nick)) && (must_be_owner)) {
-    dprintf(idx, MISC_NOSUCHCMD);
+    dprintf(idx, "%s", MISC_NOSUCHCMD);
     return;
   }
   putlog(LOG_CMDS, "*", "#%s# set %s", dcc[idx].nick, msg);
@@ -2916,7 +2916,7 @@ static void cmd_loadmod(struct userrec *u, int idx, char *par)
   const char *p;
 
   if (!(isowner(dcc[idx].nick)) && (must_be_owner)) {
-    dprintf(idx, MISC_NOSUCHCMD);
+    dprintf(idx, "%s", MISC_NOSUCHCMD);
     return;
   }
   if (!par[0]) {
@@ -2938,7 +2938,7 @@ static void cmd_unloadmod(struct userrec *u, int idx, char *par)
   char *p;
 
   if (!(isowner(dcc[idx].nick)) && (must_be_owner)) {
-    dprintf(idx, MISC_NOSUCHCMD);
+    dprintf(idx, "%s", MISC_NOSUCHCMD);
     return;
   }
   if (!par[0])
@@ -3204,7 +3204,7 @@ static void cmd_modules(struct userrec *u, int idx, char *par)
   } else {
     bot = newsplit(&par);
     if ((ptr = nextbot(bot)) >= 0)
-      dprintf(ptr, "v %s %s %d:%s\n", botnetnick, bot, dcc[idx].sock,
+      dprintf(ptr, "v %s %s %ld:%s\n", botnetnick, bot, dcc[idx].sock,
               dcc[idx].nick);
     else
       dprintf(idx, "No such bot online.\n");

src/dcc.c
@@ -593,8 +593,8 @@ static int dcc_bot_check_digest(int idx, char *remote_digest)
 
   MD5_Init(&md5context);
 
-  egg_snprintf(digest_string, 33, "<%lx%x@", (long) getpid(),
-               (unsigned int) dcc[idx].timeval);
+  egg_snprintf(digest_string, 33, "<%lx%lx@", (long) getpid(),
+               (unsigned long) dcc[idx].timeval);
   MD5_Update(&md5context, (unsigned char *) digest_string,
              strlen(digest_string));
   MD5_Update(&md5context, (unsigned char *) botnetnick, strlen(botnetnick));
@@ -722,7 +722,7 @@ static void dcc_chat_pass(int idx, char *buf, int atr)
       dprintf(idx, "badpass\n");
       dcc[idx].type = old;
     } else
-      dprintf(idx, DCC_HOUSTON);
+      dprintf(idx, "%s", DCC_HOUSTON);
     putlog(LOG_MISC, "*", DCC_BADLOGIN, dcc[idx].nick,
            dcc[idx].host, dcc[idx].port);
     if (dcc[idx].u.chat->away) {        /* su from a dumb user */
@@ -1375,10 +1375,10 @@ static void dcc_telnet_hostresolved(int i)
       sockname_t name;
       name.addrlen = sizeof(name.addr);
       if (getsockname(dcc[i].sock, &name.addr.sa, &name.addrlen) < 0)
-        debug2("dcc: dcc_telnet_hostresolved(): getsockname() socket %i error %s", dcc[i].sock, strerror(errno));
+        debug2("dcc: dcc_telnet_hostresolved(): getsockname() socket %ld error %s", dcc[i].sock, strerror(errno));
       setsnport(name, 0);
       if (bind(dcc[j].sock, &name.addr.sa, name.addrlen) < 0)
-        debug2("dcc: dcc_telnet_hostresolved(): bind() socket %i error %s", dcc[j].sock, strerror(errno));
+        debug2("dcc: dcc_telnet_hostresolved(): bind() socket %ld error %s", dcc[j].sock, strerror(errno));
       setsnport(dcc[j].sockname, 113);
       if (connect(dcc[j].sock, &dcc[j].sockname.addr.sa,
           dcc[j].sockname.addrlen) < 0 && (errno != EINPROGRESS)) {
@@ -1600,7 +1600,7 @@ static void dcc_telnet_id(int idx, char *buf, int atr)
     dcc[idx].timeval = now;
     dprintf(idx, "\n");
     dprintf(idx, IRC_TELNET, botnetnick);
-    dprintf(idx, IRC_TELNET1);
+    dprintf(idx, "%s", IRC_TELNET1);
     dprintf(idx, "\nEnter the handle you would like to use.\n");
     return;
   }
@@ -1777,7 +1777,7 @@ static void dcc_telnet_pass(int idx, int atr)
      */
     putlog(LOG_BOTS, "*", "Challenging %s...", dcc[idx].nick);
     /* Prefix with \n in case of newline-less ending stealth_prompt */
-    dprintf(idx, "\npassreq <%x%x@%s>\n", getpid(), dcc[idx].timeval, botnetnick);
+    dprintf(idx, "\npassreq <%x%lx@%s>\n", getpid(), (unsigned long)dcc[idx].timeval, botnetnick);
     dcc[idx].type = old;
   } else {
     /* NOTE: The MD5 digest used above to prevent cleartext passwords being
@@ -1892,7 +1892,7 @@ static void dcc_telnet_new(int idx, char *buf, int x)
     dcc[idx].type = &DCC_TELNET_PW;
     if (make_userfile) {
       dprintf(idx, "\nYOU ARE THE MASTER/OWNER ON THIS BOT NOW\n");
-      dprintf(idx, IRC_LIMBO);
+      dprintf(idx, "%s", IRC_LIMBO);
       putlog(LOG_MISC, "*", DCC_INSTCOMPL, buf);
       make_userfile = 0;
       write_userfile(-1);
@@ -2271,7 +2271,7 @@ void eof_timeout_dcc_ident(int idx, const char *s)
   for (i = 0; i < dcc_total; i++)
     if ((dcc[i].type == &DCC_IDENTWAIT) &&
         (dcc[i].sock == dcc[idx].u.ident_sock)) {
-      putlog(LOG_MISC, "*", s);
+      putlog(LOG_MISC, "*", "%s", s);
       snprintf(buf, sizeof buf, "telnet@%s", dcc[idx].host);
       dcc_telnet_got_ident(i, buf);
     }
@@ -2320,7 +2320,7 @@ static void dcc_telnet_got_ident(int i, char *host)
       break;
   dcc[i].u.other = 0;
   if (dcc_total == idx) {
-    putlog(LOG_MISC, "*", DCC_LOSTIDENT);
+    putlog(LOG_MISC, "*", "%s", DCC_LOSTIDENT);
     killsock(dcc[i].sock);
     lostdcc(i);
     return;
@@ -2399,7 +2399,7 @@ static void dcc_telnet_got_ident(int i, char *host)
     /* Show here so it doesn't interfere with newline-less stealth_prompt */
     if (allow_new_telnets)
       dprintf(i, "(If you are new, enter 'NEW' here.)\n");
-    dprintf(i, stealth_prompt);
+    dprintf(i, "%s", stealth_prompt);
   } else {
     dprintf(i, "\n\n");
     sub_lang(i, MISC_BANNER);

src/dccutil.c
@@ -149,15 +149,14 @@ char *add_cr(char *buf)
 
 extern void (*qserver) (int, char *, int);
 
-void dprintf EGG_VARARGS_DEF(int, arg1)
+ATTRIBUTE_FORMAT(printf,2,3)
+void dprintf(int idx, const char *format, ...)
 {
   char buf[LOGLINEMAX];
-  char *format;
-  int idx, len;
+  int len;
   va_list va;
 
-  idx = EGG_VARARGS_START(int, arg1, va);
-  format = va_arg(va, char *);
+  va_start(va, format);
 
   egg_vsnprintf(buf, LOGLINEMAX-1, format, va);
   va_end(va);
@@ -218,14 +217,14 @@ void dprint(int idx, char *buf, int len)
   }
 }
 
-void chatout EGG_VARARGS_DEF(char *, arg1)
+ATTRIBUTE_FORMAT(printf,1,2)
+void chatout(const char *format, ...)
 {
   int i, len;
-  char *format;
   char s[601];
   va_list va;
 
-  format = EGG_VARARGS_START(char *, arg1, va);
+  va_start(va, format);
 
   egg_vsnprintf(s, 511, format, va);
   va_end(va);
@@ -243,16 +242,14 @@ void chatout EGG_VARARGS_DEF(char *, arg1)
 
 /* Print to all on this channel but one.
  */
-void chanout_but EGG_VARARGS_DEF(int, arg1)
+ATTRIBUTE_FORMAT(printf,3,4)
+void chanout_but(int x, int chan, const char *format, ...)
 {
-  int i, x, chan, len;
-  char *format;
+  int i, len;
   char s[601];
   va_list va;
 
-  x = EGG_VARARGS_START(int, arg1, va);
-  chan = va_arg(va, int);
-  format = va_arg(va, char *);
+  va_start(va, format);
 
   egg_vsnprintf(s, 511, format, va);
   va_end(va);
@@ -615,7 +612,7 @@ void do_boot(int idx, char *by, char *reason)
 {
   int files = (dcc[idx].type != &DCC_CHAT);
 
-  dprintf(idx, DCC_BOOTED1);
+  dprintf(idx, "%s", DCC_BOOTED1);
   dprintf(idx, DCC_BOOTED2, files ? "file section" : "bot",
           by, reason[0] ? ": " : ".", reason);
   /* If it's a partyliner (chatterer :) */

commit b5d57c1e9954b2c756534556ac1cf932fb51b254

Fix shareout crash #1324 (see fb9f52ca)
Bug was introduced in faec9000

Found by: ploxxusVulgaris
Patch by: thommey

Fixes: #1324

src/mod/module.h
@@ -177,7 +177,7 @@ typedef void (*chanout_butfunc)(int, int, const char *, ...) ATTRIBUTE_FORMAT(pr
 #define flagrec_eq ((int(*)(struct flag_record*,struct flag_record *))global[66])
 #define flagrec_ok ((int(*)(struct flag_record*,struct flag_record *))global[67])
 /* 68 - 71 */
-#define shareout (*(Function *)(global[68]))
+#define shareout (*(Function*)(global[68]))
 #define dprintf ((dprintffunc)(global[69]))
 #define chatout ((chatoutfunc)(global[70]))
 #define chanout_but ((chanout_butfunc)(global[71]))