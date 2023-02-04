bec15cb6f69a5c671a5d3b242d529c59e8c7b10e
Geo <geo@eggheads.org>
2023-02-04 18:01:22 -0500

    Initial commit

---
+++ doc/sphinx_source/using/autoscripts.rst
@@ -0,0 +1,113 @@
+Eggdrop Autoscripts
+===================
+
+Since it's inception, users have needed to load a Tcl script into Eggdrop by downloading a Tcl file, editing the file to customize settings, and then sourceing that file in the config file. In v1.10, the Autoscripts system was added to make this process a little more user-friendly. The autoscripts system helps by:
+
+* Centralizing commonly-used scripts in a single location
+* Allowing scripts to be downloaded via the partyline
+* Allowing script settings to be configured via the partyline
+* Allowing user-written scripts to be managed by the autoscripts system
+* Providing a documented API to write autoscripts-compatible scripts
+
+Autoscripts usage
+-----------------
+To view available autoscript commands, type `.script` on the partyline. The following sub-commands are available for use with script:
+
+remote
+^^^^^^
+This command will list scripts hosted on the Eggdrop website that are available to be downloaded and installed on your Eggdrop.
+
+fetch <script>
+^^^^^^^^^^^^^^
+This command will download the specified script from the Eggdrop website and place it into the autoscript/ directory.
+
+list
+^^^^
+This command will list scripts locallt present in the autoscripts/ directory, available to be configured and loaded.
+
+config <script>
+^^^^^^^^^^^^^^^
+This command will list settings available for configuration for the provided script.
+
+set <script> <setting>
+^^^^^^^^^^^^^^^^^^^^^^
+This command will set `setting` for `script` to the provided value.
+
+load <script>
+^^^^^^^^^^^^^
+This command will activate the script for use.
+
+unload <script>
+^^^^^^^^^^^^^^^
+This command will prevent the script from being loaded the next time Eggdrop starts. To fully unload a script, Eggdrop must be restarted!
+
+clean <script>
+^^^^^^^^^^^^^^
+This command will delete the script from the filesystem. After running this command, you will have to re-download and re-configure the script if you wish to use it again.
+
+
+Autoscripts File Structure
+--------------------------
+An autoscripts package requires (minimum) two files: the Tcl script, and a json manifest file. 
+
+Tcl File
+^^^^^^^^
+Nothing new or novel here; this is where your Tcl code goes. The one change to this file is that any setting intended should now be located in the manifest.json file, not the Tcl script file. All variables will be added to the global namespace. For this reason, it is recommended that variables have unique names to avoid collisions. This is commonly done by prefixing the variable name with the script name or abbreviation. For example, a script called myscript.tcl might avoid using a variable calld `name` and instead use `myscript_name` or `ms_name`.
+
+Manifest.json
+^^^^^^^^^^^^^
+Every autoscripts package must have a manifest.json file. This file contains metadata for the script such as version and description information, as well as the user-configurable settings for use with th script. A simple example of a manifest.json file is as follows::
+
+  {
+    "name": "woobie",
+    "version_major": 1,
+    "version_minor": 0,
+    "description": "An example script to help developers write autoscript packages",
+    "long_description": "This is an example script to help understand the autoscript system. Yeah, it doesn't really do anything, but that's besides the point. It could, and that should be enough for anyone"
+    "config": {
+      "loaded": 0,
+      "udefflag":"myscript"
+      "requires": null,
+      "vars": {
+        "woobie_dict": {
+          "description": "A setting that accepts a dict as a value",
+          "value": "{quiet q}"
+        },
+        "woobie_setting": {
+          "description": "A normal setting to enable or disable something",
+          "value": "1"
+        },
+        "woobie_string": {
+          "description": "A setting taking a string, like a filename or something",
+          "value": "woobie"
+        },
+        "woobie(array)": {
+          "description": "A setting that is set as an array",
+          "value":"another string"
+        }
+      }
+    }
+  }
+
+:name: The name of the script. Must match the script name (if the script is foo.tcl, then this must be foo)
+:version_major: The major version integer (ie, 1 for 1.6)
+:version_minor: The minor version integer (ie, 6 for 1.6)
+:description: A one-line summary of what the script does. This will be shown when available scripts are listed on the partyline via .script list.
+:long_description: A longer description of what the script does, similar to a README. This will be shown when a script is viewed via .script config.
+:config-loaded: Whether this script is currently loaded or not. It should be default set to 0.
+:config-udefflag: Any user-defined channel settings used by the script. This is displayed when configuration settings are displayed to the user on the partyline.
+:config-requires: Any Tcl package required for use by the script, such as tcllib or json.
+:config-vars-<varname>: A setting intended to be modified by the user. The 'description' field should describe what the setting does, and the 'value' field stores the current value. These settings are displayed when the configuration settings are displayed to the user on the partyline.
+:config-vars-<varname>-description: A description of the setting, displayed in the configuration listing for the script
+:config-vars-<varname>-value: The value thee setting is set to
+
+File placement
+^^^^^^^^^^^^^^
+Autoscript files are stored in the autoscript directory. The path structure is eggdrop/autoscript/<scriptname>/[script files]. If the autoscript `fetch` command is used, a .tgz file will be downloaded and extracted to the proper location automatically. If you wish to manually add a script, create a directory with the same name as the script, and then place the script and manifest files inside the directory. The directory name must exactly match the script name (without the .tcl extension)! If the Tcl script to be loaded is called `myscript_goodversion_specialfeature.tcl`, then the directory must also called `myscript_goodversion_specialfeature`.
+
+Development hints
+-----------------
+
+* An autoscript should not require a user to manually open the script in an editor for any reason. Design your script as such!
+* Use `user defined channel flags <https://docs.eggheads.org/using/tcl-commands.html#setudef-flag-int-str-name>` to enable/disable a script for a particular channel, they're easy!
+* Variables used in autoscripts are placed into the global namespace. Make them unique to prevent collisions! We recommend prefixing the script name in front of a variable, such as myscript_setting or ms_setting.
---
+++ scripts/autoscripts.tcl
@@ -0,0 +1,353 @@
+package require json
+package require json::write
+package require http
+package require tls
+
+
+bind dcc * egg parse_egg
+bind dcc * scripts parse_egg
+
+set eggdir "autoscripts"
+
+set jsondict [dict create]
+
+#Read all JSON script files
+proc readjsonfile {} {
+  global jsondict
+  global eggdir
+  set jsonstring "\["
+### How to get filepath properly
+  foreach dirname [glob -nocomplain -type d -directory $eggdir *] {
+putlog "doing $dirname"
+    if {![file exists $dirname/manifest.json]} {
+      file delete -force $dirname
+      putlog "$dirname missing manifest.json, deleting"
+    } else {
+      set fs [open $dirname/manifest.json r]
+      if {[string length $jsonstring] > 1} {
+        append jsonstring ","
+      }
+      set filestring [read $fs]
+      append jsonstring $filestring
+      close $fs
+    }
+  }
+  append jsonstring "\]"
+  set jsondict [json::json2dict $jsonstring]
+}
+
+# Write a script's JSON content to file
+proc write_json {script jstring} {
+  global eggdir
+  set fs [open $eggdir/${script}/manifest.json w]
+  puts $fs $jstring
+  close $fs
+}
+
+# Send an HTTP request. Type 0 for text, 1 for binary
+proc send_http {url type} {
+  global eggdir
+  http::register https 443 [list ::tls::socket -autoservername true]
+  set req [http::config -useragent "eggdrop"]
+  if {$type} {
+    set fs [open $eggdir/[lindex [split $url /] end] w]
+    catch {set req [http::geturl $url -binary 1 -channel $fs]} error
+    close $fs
+  } else {
+    catch {set req [http::geturl $url -timeout 10000]} error
+  }
+  set status [http::status $req]
+  if {$status != "ok"} {
+    putlog "HTTP request error: $error"
+    return
+  }
+  set data [http::data $req]
+  ::http::cleanup $req
+  return $data
+}
+
+# Read all manifest files
+proc loadscripts {} {
+  global jsondict
+  global eggdir
+  foreach scriptentry $jsondict {
+    if [dict get $scriptentry config loaded] {
+      if {[dict exists $scriptentry config vars]} {
+        foreach configvar [dict keys [dict get $scriptentry config vars] *] {
+          uplevel #0 set $configvar [dict get $scriptentry config vars $configvar value]
+        }
+      }
+      if {[catch {uplevel #0 source $eggdir/[dict get $scriptentry name]/[dict get $scriptentry name].tcl} err]} {
+        putlog "Error loading [dict get $scriptentry]: $err"
+        return
+      }
+    }
+  }
+}
+
+# Initial function called by .egg command, sends to proper proc based on args
+proc parse_egg {hand idx text} {
+	set args [split $text]
+	set args [lassign $args subcmd arg1 arg2]
+	if {$subcmd in {list remote}} {
+		egg_$subcmd $idx
+	} elseif {$subcmd in {config fetch clean}} {
+		if {$arg1 eq ""} {
+			putdcc $idx "Missing parameter, must be $::lastbind $subcmd scriptName"
+		} else {
+			egg_$subcmd $idx $arg1
+		}
+	} elseif {$subcmd in {load unload}} {
+		if {$arg1 eq ""} {
+			putdcc $idx "Missing parameter, must be $::lastbind $subcmd scriptName"
+		} else {
+			egg_load $idx $arg1 [expr {$subcmd eq "load"}]
+		}
+	} elseif {$subcmd in {set}} {
+		if {![llength $args]} {
+			putdcc $idx "Missing parameter, must be $::lastbind $subcmd scriptName settingName newSettingValue"
+		} else {
+			egg_$subcmd $idx $arg1 $arg2 [join $args]
+		}
+	} else {
+		putdcc $idx "Missing or unknown subcommand, must be $::lastbind set, config, load, unload, remote, fetch, clean"
+	}
+}
+
+# List scripts that are locally present in .egg
+proc egg_list {idx} {
+  global jsondict
+  readjsonfile
+  putdcc $idx "\nThe following scripts are available for configuration:"
+  putdcc $idx "------------------------------------------------------"
+  foreach script $jsondict {
+    set loaded [expr {[dict get $script config loaded] == 1 ? "\[X\]" : "\[ \]"}]
+    putdcc $idx "* $loaded [dict get $script name] (v[dict get $script version_major].[dict get $script version_minor]) - [dict get $script description]"
+    if {[dict exists $script config requires] && [string length [dict get $script config requires]]} {
+      foreach pkg [dict get $script config requires] {
+        if {[catch {[package require [dict get $script $pkg]]}]} {
+          putdcc $idx "      ( ^ Must install Tcl $pkg package before loading)"
+        }
+      }
+    }
+  }
+}
+
+# Load or unload a script and update JSON field
+proc egg_load {idx script loadme} {
+  global jsondict
+  global eggdir
+  set found 0
+  readjsonfile
+  foreach scriptentry $jsondict {
+    if [string match $script [dict get $scriptentry name]] {
+      set found 1
+      if {$loadme} {
+        if {[dict exists $scriptentry config vars]} {
+          foreach configvar [dict keys [dict get $scriptentry config vars] *] {
+            uplevel #0 set $configvar [dict get $scriptentry config vars $configvar value]
+          }
+        }
+        if {[catch {uplevel #0 source $eggdir/${script}/${script}.tcl} err]} {
+          putdcc $idx "Error loading ${script}: $err"
+          return
+        }
+        dict set scriptentry config loaded 1
+        putdcc $idx "* Loaded $script."
+      } else {
+        dict set scriptentry config loaded 0
+        putdcc $idx "* Removed $script from being loaded. Restart Eggdrop to complete."
+      }
+      write_json $script [compile_json {dict config {dict vars {dict * dict}}} $scriptentry]
+      readjsonfile
+      break
+    }
+  }
+  if {!$found} {
+    putdcc $idx "* $script not found."
+  }
+}
+
+# List variables available for a script
+proc egg_config {idx script} {
+  global jsondict
+  foreach scriptentry $jsondict {
+    if {[string match $script [dict get $scriptentry name]]} {
+      if {[dict exists $scriptentry long_description]} {
+        putdcc $idx "\n* [dict get $scriptentry long_description]\n\n"
+      }
+      if {![dict exists $scriptentry config vars]} {
+        putdcc $idx "* No variables are available to configure for $script"
+      } else {
+        putdcc $idx "* The following config options are available for $script:"
+        putdcc $idx "---------------------------------------------------------"
+        foreach configvar [dict keys [dict get $scriptentry config vars] *] {
+          putdcc $idx "* $configvar - [dict get $scriptentry config vars $configvar description] (current value: [dict get $scriptentry config vars $configvar value])"
+        }
+        if {[dict exists $scriptentry config udefflag]} {
+          putdcc $idx ""
+          putdcc $idx "* Enable for a channel via .chanset <channel> +[dict get $scriptentry config udefflag]"
+        }
+      }
+    }
+  }
+}
+
+# Set a variable for a script
+proc egg_set {idx script setting value} {
+  global jsondict
+  foreach scriptentry $jsondict {
+    if {[string match $script [dict get $scriptentry name]]} {
+      if [dict exists $scriptentry config vars $setting] {
+        dict set scriptentry config vars $setting value $value
+        write_json $script [compile_json {dict config {dict vars {dict * dict}}} $scriptentry]
+        putdcc $idx "* ${script}: Variable \"$setting\" set to \"${value}\""
+        readjsonfile        
+      }
+    }
+  }
+}
+
+# Pull down remote Tcl file listing
+# For future eggheads- 100 is the maximum WP supports without doing pagination
+proc egg_remote {idx} {
+  set jsondata [send_http "https://www.eggheads.org/wp-json/wp/v2/media?mime_type=application\/x-gzip&orderby=slug&per_page=100&order=asc" 0]
+  set datadict [json::json2dict $jsondata]
+  putdcc $idx "* Retrieving script list, please wait..."
+  putdcc $idx "Scripts available for download:"
+  putdcc $idx "-------------------------------"
+  foreach scriptentry $datadict {
+    putdcc $idx "* [dict get $scriptentry slug] ([lindex [regexp -inline {\n.*<p>([^<>]*)</p>} [dict get $scriptentry description rendered]] 1])"
+  }
+  putdcc $idx "\n"
+  putdcc $idx "* Type '.egg fetch <scriptname>' to download a script"
+}
+
+# Download a script from the eggheads repository
+proc egg_fetch {idx script} {
+  global eggdir
+  try {
+    set results [exec which tar]
+    set status 0
+  } trap CHILDSTATUS {results options} {
+    if {lindex [dict get $options -errorcode] 2} {
+      putdcc $idx "* ERROR: This feature is not available (tar not detected on this system, cannot extract a downloaded file)."
+      return
+    }
+  }
+  if {[file isdirectory $eggdir/$script]} {
+    putdcc $idx "* Script directory already exists. Not downloading again!"
+    return
+  }
+### CHECK IF IT IS ON SITE FIRST
+  putdcc $idx "* Downloading, please wait..."
+  set jsondata [send_http "https://www.eggheads.org/wp-json/wp/v2/media?mime_type=application\/x-gzip" 0]
+  set datadict [json::json2dict $jsondata]
+  foreach scriptentry $datadict {
+    if {[string match $script [dict get $scriptentry slug]]} {
+      send_http [dict get $scriptentry source_url] 1
+      file mkdir $eggdir/[dict get $scriptentry slug]
+      exec tar -zxf $eggdir/[dict get $scriptentry slug].tgz -C $eggdir/[dict get $scriptentry slug]
+      file delete $eggdir/[dict get $scriptentry slug].tgz
+      putdcc $idx "* [dict get $scriptentry slug] downloaded."
+      putdcc $idx "* Use '.egg load [dict get $scriptentry slug]' to load and '.egg config [dict get $scriptentry slug]' to configure."
+      readjsonfile
+    }
+  }
+}
+
+proc egg_clean {idx script} {
+  global eggdir
+  if {[file isdirectory $eggdir/$script]} {
+    file delete -force $eggdir/$script
+    putdcc $idx "* $script deleted"
+  } else {
+    putdcc %idx "* $script not found"
+  }
+}
+
+# Yikes.
+proc compile_json {spec data} {
+	while [llength $spec] {
+		set type [lindex $spec 0]
+		set spec [lrange $spec 1 end]
+
+		switch -- $type {
+			dict {
+				lappend spec * unknown
+
+				set json {}
+				foreach {key val} $data {
+					foreach {keymatch valtype} $spec {
+						if {[string match $keymatch $key]} {
+							if {$key in {name displayname}} { set valtype string }
+							lappend json [subst {"$key":[compile_json $valtype $val]}]
+							break
+						}
+					}
+				}
+				return "{[join $json ,]}"
+			}
+			list {
+				if {![llength $spec]} {
+					set spec unknown
+				} else {
+					set spec [lindex $spec 0]
+				}
+				set json {}
+				foreach {val} $data {
+					lappend json [compile_json $spec $val]
+				}
+				return "\[[join $json ,]\]"
+			}
+			number {
+				if {$data eq "" || $data eq "null"} {
+					return null
+				} else {
+					return $data
+				}
+			}
+			string {
+				if {$data eq "" || $data eq "null"} {
+					return null
+				} else {
+					set new ""
+					for {set i 0} {$i < [string length $data]} {incr i} {
+						set c [string index $data $i]
+						set cc [scan $c %c]
+						if {$cc < 0x7e && $c ne "\\" && $cc >= 0x20 && $c ne "\""} {
+							append new $c
+						} else {
+							append new "\\u[format %04x $cc]"
+						}
+					}
+					return "\"$new\""
+				}
+			}
+			unknown {
+				if {$data eq "" || $data eq "null"} {
+					return null
+				} elseif {[string is double -strict $data] && ![string equal -nocase $data infinity] && ![string equal -nocase $data nan] && ![string equal -nocase $data -infinity]} {
+					return $data
+				} else {
+					set new ""
+					for {set i 0} {$i < [string length $data]} {incr i} {
+						set c [string index $data $i]
+						set cc [scan $c %c]
+						if {$cc < 0x7e && $c ne "\\" && $cc >= 0x20 && $c ne "\""} {
+							append new $c
+						} else {
+							append new "\\u[format %04x $cc]"
+						}
+					}
+					return "\"$new\""
+				}
+			}
+		}
+	}
+}
+
+if {![file exists autoscripts]} {file mkdir autoscripts}
+readjsonfile
+loadscripts
+putlog "Loading foo.tcl"
