e2713f6a8d3a89bcd6006c157bb35c4152b422f7
Geo <geo@eggheads.org>
2023-02-13 21:22:41 -0500

    Update version strings to 1.9.5

--- src/version.h
+++ src/version.h
@@ -26,6 +26,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#define EGG_STRINGVER "1.9.4"
-#define EGG_NUMVER 1090405
-#define EGG_PATCH "raisetcl"
+#define EGG_STRINGVER "1.9.5"
+#define EGG_NUMVER 1090500
+#define EGG_PATCH "alpha"
