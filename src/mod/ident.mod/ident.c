879b1a24c2b3104ac8dc7a988fd3e52e0d64ecda
Michael Ortmann <keine email>
2023-08-29 19:29:51 +0200

    bump ident.mod version to 1.0, for its stable now

--- src/mod/ident.mod/ident.c
+++ src/mod/ident.mod/ident.c
@@ -292,7 +292,7 @@ char *ident_start(Function *global_funcs)
 {
   global = global_funcs;
 
-  module_register(MODULE_NAME, ident_table, 0, 9);
+  module_register(MODULE_NAME, ident_table, 1, 0);
 
   if (!module_depend(MODULE_NAME, "eggdrop", 109, 0)) {
     module_undepend(MODULE_NAME);
