fa2936a667c86941df6e5be8833c1e30535f70e3
Michael Ortmann <keine email>
2024-02-12 11:56:08 +0100

    Include c header

--- src/mod/zig.mod/zig.zig
+++ src/mod/zig.mod/zig.zig
@@ -4,10 +4,19 @@
 //
 // Copyright (c) 2024 Michael Ortmann
 
+const MODULE_NAME: []u8 = "zig";
+
+const c = @cImport({
+    @cDefine("STATIC", "1"); // TODO
+    @cInclude("src/mod/module.h");
+});
+
 const std = @import("std");
 
-export fn zig_start() ?[*]const u8 {
-  const stdout = std.io.getStdOut().writer();
-  stdout.print("Hello, {s}!\n", .{"world"}) catch return null;
-  return "WIP".ptr;
+export fn zig_start(global_funcs: c.Function) ?[*]const u8 {
+    _ = global_funcs;
+    // module_register(MODULE_NAME, 0, 0, 1);
+
+    std.io.getStdOut().writer().print("hello from zig.mod zig_start()\n", .{}) catch return null;
+    return "WIP".ptr;
 }
