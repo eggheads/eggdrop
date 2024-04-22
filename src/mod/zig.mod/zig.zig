69895f5b39121167ded65fba8b8c96dbf516cbd9
Michael Ortmann <keine email>
2024-04-22 15:20:30 +0200

    (WIP) Add version, enhance global_func and add modcall / zig_table

--- src/mod/zig.mod/zig.zig
+++ src/mod/zig.mod/zig.zig
@@ -6,6 +6,7 @@
 
 const MODULE_NAME = "zig";
 
+const builtin = @import("builtin");
 const c = @cImport({
     @cDefine("STATIC", "1"); // TODO
     @cInclude("src/mod/module.h");
@@ -14,18 +15,37 @@ const std = @import("std");
 
 const global_funcs = extern struct {
     // 0 - 3
-    mod_malloc: *const fn () c_int,
-    mod_free: *const fn () c_int,
-    egg_context: *const fn () c_int,
-    module_rename: *const fn () c_int,
+    mod_malloc: *const fn () callconv(.C) void,
+    mod_free: *const fn () callconv(.C) void,
+    egg_context: *const fn () callconv(.C) void,
+    module_rename: *const fn () callconv(.C) void,
     // 4 - 7
-    module_register: *const fn ([*]const u8, c_int, c_int, c_int) c_int,
+    module_register: *const fn ([*]const u8, *const modcall, c_int, c_int) callconv(.C) c_int,
 };
 
+const modcall = extern struct {
+    start: ?*const fn (*global_funcs) callconv(.C) ?[*]const u8, // MODCALL_START
+    close: ?*const fn () callconv(.C) void, // MODCALL_CLOSE
+    expmem: ?*const fn () callconv(.C) void, // MODCALL_EXPMEM
+    report: ?*const fn () callconv(.C) void, // MODCALL_REPORT
+};
+
+export fn zig_report(idx: c_int, details: c_int) void {
+    _ = idx;
+    _ = details;
+}
+
 export fn zig_start(global: *global_funcs) ?[*]const u8 {
-    _ = global.module_register(MODULE_NAME.ptr, 0, 0, 1);
+    const zig_table = modcall{
+        .start = null, //zig_start,
+        .close = null,
+        .expmem = null,
+        .report = null,
+    };
+    _ = global.module_register(MODULE_NAME.ptr, &zig_table, 0, 1);
 
     std.io.getStdOut().writer().print("hello from zig.mod zig_start()\n", .{}) catch return null;
+    std.io.getStdOut().writer().print("zig version: {s}\n", .{builtin.zig_version_string}) catch return null;
     // return "WIP".ptr;
     return null;
 }
