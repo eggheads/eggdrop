287d65a057dd2893c0a0c6e8f1b717ad0afbdf77
Michael Ortmann <keine email>
2024-04-22 15:41:06 +0200

    Do not call a modules restart() function if it is null pointer

--- src/main.c
+++ src/main.c
@@ -900,7 +900,10 @@ static void mainloop(int toplevel)
       for (p = module_list; p; p = p->next) {
         if (p->funcs) {
           startfunc = p->funcs[MODCALL_START];
-          startfunc(NULL);
+          if (startfunc)
+            startfunc(NULL);
+          else
+            debug2("module: %s: %s", p->name, MOD_NOSTARTDEF);
         }
       }
 
