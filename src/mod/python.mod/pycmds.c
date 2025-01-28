7b74fa8279cd2752ea39417dbd589083406f97a7
Michael Ortmann <41313082+michaelortmann@users.noreply.github.com>
2025-01-28 16:10:24 +0100

    Fix includes

--- src/mod/python.mod/pycmds.c
+++ src/mod/python.mod/pycmds.c
@@ -20,10 +20,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #define PY_SSIZE_T_CLEAN
-#include <Python.h>
-#include <datetime.h>
-#include <tcl.h>
-#include "src/mod/module.h"
 
 typedef struct {
   PyObject_HEAD
