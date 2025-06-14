f3d4c8393a8386960b004ee479732d7b921f4bb8
Geo <vanosg@users.noreply.github.com>
2025-06-14 13:23:20 -0400

    fix optional args

--- src/mod/server.mod/tclserv.c
+++ src/mod/server.mod/tclserv.c
@@ -583,7 +583,7 @@ static int tcl_server STDVAR {
   BADARGS(2, 5, " subcommand ?host ?port? ?password?");
   if (!strcmp(argv[1], "add")) {
     if (argc < 3) {
-      Tcl_SetResult(irp, "wrong # args: should be \"server add host ?port? ?password?\"", TCL_STATIC);
+      Tcl_SetResult(irp, "wrong # args: should be \"server add host ?port ?password??\"", TCL_STATIC);
       return TCL_ERROR;
     }
     ret = add_server(argv[2], argc >= 4 && argv[3] ? argv[3] : "", argc >= 5 && argv[4] ? argv[4] : "");
