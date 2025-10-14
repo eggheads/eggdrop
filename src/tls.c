c47963e882b2911c3a15350822b3ed76fccb125c
Michael Ortmann <ortmann@posteo.de>
2025-10-14 15:02:38 +0200

    Enhance tls error logging

--- src/tls.c
+++ src/tls.c
@@ -883,6 +883,9 @@ static void ssl_info(const SSL *ssl, int where, int ret)
              (where & SSL_CB_READ) ? "read" : "write",
              SSL_alert_type_string_long(ret),
              SSL_alert_desc_string_long(ret));
+      if (!strcmp(SSL_alert_type_string(ret), "F") &&
+          !strcmp(SSL_alert_desc_string(ret), "RO"))
+        putlog(LOG_MISC, "*", "ERROR: No tls port?");
     } else {
       /* Ignore close notify warnings */
       debug1("TLS: Received close notify during %s",
