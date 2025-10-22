ed513c77e8159ca35cc4480ec21b623f02d65089
Michael Ortmann <ortmann@posteo.de>
2025-10-22 07:07:26 +0200

    Fill in values in same order of struct sockname_t

--- src/dcc.c
+++ src/dcc.c
@@ -1397,8 +1397,8 @@ static void dcc_telnet_hostresolved(int i)
     dcc[j].sock = getsock(dcc[j].sockname.family, 0);
     if (dcc[j].sock >= 0) {
       sockname_t name;
-      name.addrlen = sizeof(name.addr);
       name.family = dcc[j].sockname.family;
+      name.addrlen = sizeof(name.addr);
       if (getsockname(dcc[i].sock, &name.addr.sa, &name.addrlen) < 0)
         debug2("dcc: dcc_telnet_hostresolved(): getsockname() socket %ld error %s", dcc[i].sock, strerror(errno));
       setsnport(name, 0);
