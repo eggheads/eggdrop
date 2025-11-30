457939e8e13f5354e304a05f90be6c0551c85782
Michael Ortmann <ortmann@posteo.de>
2025-11-30 19:11:01 +0100

    Fix tls

--- src/tls.c
+++ src/tls.c
@@ -240,6 +240,7 @@ int ssl_init()
 #if OPENSSL_VERSION_NUMBER < 0x10100000L /* 1.1.0 */
     ERR_free_strings();
 #endif
+
     return -1;
   }
   ssl_files_loaded = 0;
@@ -405,6 +406,8 @@ int ssl_init()
     ssl_ctx = NULL;
     return -3;
   }
+  const unsigned char sid_ctx[] = "0";
+  SSL_CTX_set_session_id_context(ssl_ctx, sid_ctx, (sizeof sid_ctx) - 1);
   return 0;
 }
 
