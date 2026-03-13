2a504d97ab59d8e7657f12686e376b67b009cd11
thommey <thommey@gmail.com>
2026-03-13 12:02:53 -0500

    Rust test suite init

---
+++ test-rs/build.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let eggdrop_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
+
+    println!("cargo:rustc-link-search=native={}", eggdrop_dir.display());
+    println!("cargo:rustc-link-lib=static=eggdrop");
+
+    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-lib=dylib=tcl8.6");
+    println!("cargo:rustc-link-lib=dylib=z");
+    println!("cargo:rustc-link-lib=dylib=ssl");
+    println!("cargo:rustc-link-lib=dylib=crypto");
+    println!("cargo:rustc-link-lib=dylib=resolv");
+    println!("cargo:rustc-link-lib=dylib=m");
+    println!("cargo:rustc-link-lib=dylib=pthread");
+
+    println!("cargo:rerun-if-changed={}", eggdrop_dir.join("libeggdrop.a").display());
+}
