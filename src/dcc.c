16bdb16548880a9ef262f01bebb631610ed0f6f6
Michael Ortmann <ortmann@posteo.de>
2026-05-18 08:32:56 +0200

    Proper memleak fix

--- src/dcc.c
+++ src/dcc.c
@@ -1346,6 +1346,8 @@ void dcc_telnet_hostresolved2(int i, int idx) {
     ident_target_port = getenv("EGGDROP_TEST") ? 1113 : 113;
 
   snprintf(userhost, sizeof userhost, "telnet@%s", dcc[i].host);
+  changeover_dcc(i, &DCC_IDENTWAIT, 0);
+
   /* Skip ident lookup if disabled */
   if (identtimeout <= 0) {
     dcc[i].u.ident_sock = dcc[idx].sock;
@@ -1353,7 +1355,6 @@ void dcc_telnet_hostresolved2(int i, int idx) {
     return;
   }
 
-  changeover_dcc(i, &DCC_IDENTWAIT, 0);
   dcc[i].timeval = now;
   dcc[i].u.ident_sock = dcc[idx].sock;
   sock = -1;
@@ -1451,13 +1452,6 @@ static void dcc_telnet_hostresolved(int i)
     }
   }
 #endif
-
-  /* dcc type is DNSWAIT
-   * free its dcc[i].u.dns_info
-   * because lostdcc() is not called on it
-   * but its type is changed and dcc[i].u is reused
-   */
-  dcc[i].type->kill(i, dcc[i].u.other);
 
   /* Skip ident lookup for public script listeners */
   if ((dcc[idx].status & LSTN_PUBLIC) && !strcmp(dcc[idx].nick, "(script)")) {
